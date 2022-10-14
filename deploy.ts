dc3ccefe886ac467e2682ff03b577829e97b86fe
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2022-10-14 20:06:30 +0300

    Generate mod changelogs

--- deploy.ts
+++ deploy.ts
@@ -35,9 +35,28 @@ function JSONstringifyOrder(obj: any, space: number) {
     return JSON.stringify(obj, Array.from(allKeys).sort(), space);
 }
 
+function gitExec(args: string[]) {
+    const result = child_process.spawnSync('git', args, { encoding: 'utf8' });
+    if (result.status !== 0) {
+        throw new Error('git ' + args.join(' ') + ' failed with status ' + result.status + ' and stderr ' + result.stderr);
+    }
+
+    if (result.stderr) {
+        console.warn('git ' + args.join(' ') + ' produced stderr ' + result.stderr);
+    }
+
+    return result.stdout;
+}
+
 function getModCreatedTime(modId: string) {
-    const command = `git log --diff-filter=A --format=%ct -1 -- mods/${modId}.wh.cpp`;
-    const time = parseInt(child_process.execSync(command, { encoding: "utf8" }), 10);
+    const time = parseInt(gitExec([
+        'log',
+        '--diff-filter=A',
+        '--format=%ct',
+        '-1',
+        '--',
+        `mods/${modId}.wh.cpp`,
+    ]), 10);
     if (isNaN(time)) {
         throw new Error(`Can't get created time for ${modId}`);
     }
@@ -46,8 +65,13 @@ function getModCreatedTime(modId: string) {
 }
 
 function getModModifiedTime(modId: string) {
-    const command = `git log --format=%ct -1 -- mods/${modId}.wh.cpp`;
-    const time = parseInt(child_process.execSync(command, { encoding: "utf8" }), 10);
+    const time = parseInt(gitExec([
+        'log',
+        '--format=%ct',
+        '-1',
+        '--',
+        `mods/${modId}.wh.cpp`,
+    ]), 10);
     if (isNaN(time)) {
         throw new Error(`Can't get modified time for ${modId}`);
     }
@@ -99,9 +123,73 @@ async function generateModCatalog() {
     return await enrichCatalog(catalog);
 }
 
+function generateModChangelog(modId: string) {
+    let changelog = '';
+
+    const modSourceUtils = new ModSourceUtils('mods');
+
+    const commits = gitExec([
+        'rev-list',
+        'HEAD',
+        '--',
+        `mods/${modId}.wh.cpp`,
+    ]).trim().split('\n');
+    const lastCommit = commits[commits.length - 1];
+
+    for (const commit of commits) {
+        const modFile = gitExec([
+            'show',
+            `${commit}:mods/${modId}.wh.cpp`,
+        ]);
+
+        const metadata = modSourceUtils.extractMetadata(modFile, 'en-US');
+
+        const commitTime = parseInt(gitExec([
+            'log',
+            '--format=%ct',
+            '-1',
+            commit,
+        ]), 10);
+
+        const commitFormattedDate = new Date(commitTime * 1000)
+            .toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
+
+        changelog += `# ${metadata.version} ([${commitFormattedDate}](https://github.com/ramensoftware/windhawk-mods/commit/${commit}))\n\n`;
+
+        if (commit !== lastCommit) {
+            const message = gitExec([
+                'log',
+                '-1',
+                '--pretty=format:%B',
+                commit,
+            ]);
+            changelog += `${message}\n`;
+        } else {
+            changelog += 'Initial release.\n';
+        }
+    }
+
+    return changelog;
+}
+
+function generateModChangelogs(modIds: string[]) {
+    const changelogDir = 'changelogs';
+    if (!fs.existsSync(changelogDir)) {
+        fs.mkdirSync(changelogDir);
+    }
+
+    for (const modId of modIds) {
+        const changelogPath = path.join(changelogDir, `${modId}.md`);
+        fs.writeFileSync(changelogPath, generateModChangelog(modId));
+    }
+}
+
 async function main() {
     const catalog = await generateModCatalog();
     fs.writeFileSync('catalog.json', JSONstringifyOrder(catalog, 4));
+
+    const modIds = Object.keys(catalog.mods);
+    generateModChangelogs(modIds);
 
     const srcPath = 'public';
     for (const file of fs.readdirSync(srcPath, { withFileTypes: true })) {
