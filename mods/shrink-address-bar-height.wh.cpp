eb6cc80014795d6a22b82280fb9e951cd26d840f
Professsional <63961221+ItsProfessional@users.noreply.github.com>
2023-03-03 22:12:03 +0530

    Shrink Address Bar Height 1.0.0 (#117)
    * Shrink Address Bar Height 1.0.0
    
    * Update shrink-address-bar-height.wh.cpp
    
    * Update shrink-address-bar-height.wh.cpp

---
+++ mods/shrink-address-bar-height.wh.cpp
@@ -0,0 +1,40 @@
+// ==WindhawkMod==
+// @id              shrink-address-bar-height
+// @name            Shrink Address Bar Height
+// @description     Shrinks the address bar height in file explorer
+// @version         1.0.0
+// @author          ItsProfessional
+// @github          https://github.com/ItsProfessional
+// @include         *
+// ==/WindhawkMod==
+
+// ==WindhawkModReadme==
+/*
+# Shrink Address Bar Height
+This mod shrinks the address bar height in file explorer.
+
+Note: This mod applies to explorer folders as well as save/open dialogs in all programs.
+
+The code is based on the implementation in [ExplorerPatcher](https://github.com/valinet/ExplorerPatcher).
+
+![Screenshot](https://raw.githubusercontent.com/ItsProfessional/Screenshots/main/Windhawk/shrink-address-bar-height.png)
+*/
+// ==/WindhawkModReadme==
+
+int (*ExplorerFrame_GetSystemMetricsForDpiOrig)(int nIndex, UINT dpi);
+int ExplorerFrame_GetSystemMetricsForDpiHook(int nIndex, UINT dpi)
+{
+    if (nIndex == SM_CYFIXEDFRAME) return 0;
+    
+    return ExplorerFrame_GetSystemMetricsForDpiOrig(nIndex, dpi);
+}
+
+
+BOOL Wh_ModInit() {
+    HMODULE hUser32 = GetModuleHandle(L"user32.dll");
+
+    void* origFunc = (void*)GetProcAddress(hUser32, "GetSystemMetricsForDpi");
+    Wh_SetFunctionHook(origFunc, (void*)ExplorerFrame_GetSystemMetricsForDpiHook, (void**)&ExplorerFrame_GetSystemMetricsForDpiOrig);
+
+    return TRUE;
+}
