6fef17804b28c2e6f440e363904dd34a8d2d176f
Professsional <63961221+ItsProfessional@users.noreply.github.com>
2023-03-05 14:23:28 +0530

    Update disable-immersive-context-menus.wh.cpp

--- mods/disable-immersive-context-menus.wh.cpp
+++ mods/disable-immersive-context-menus.wh.cpp
@@ -5,13 +5,15 @@
 // @version         1.0.0
 // @author          ItsProfessional
 // @github          https://github.com/ItsProfessional
-// @include         explorer.exe
+// @include         *
 // ==/WindhawkMod==
 
 // ==WindhawkModReadme==
 /*
 # Disable Immersive Context Menus
 This mod disables immersive context menus in file explorer.
+
+This mod applies to explorer folders as well as save/open dialogs in all programs.
 
 Note: This mod disables immersive context menus for *file explorer*, *desktop*, etc.  
 For disabling immersive context menus on the *taskbar*, use my [Non Immersive Taskbar Context Menu](https://windhawk.net/mods/classic-taskbar-context-menu) mod.
