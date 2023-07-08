32d3ac5b6685e222d14686596bb6ee8088b8847a
Alcatel <84914212+arukateru@users.noreply.github.com>
2023-07-08 15:25:52 -0400

    Update mods/force-thick-frames.wh.cpp
    Co-authored-by: Michael Maltsev <4129781+m417z@users.noreply.github.com>

--- mods/force-thick-frames.wh.cpp
+++ mods/force-thick-frames.wh.cpp
@@ -1,7 +1,7 @@
 // ==WindhawkMod==
 // @id              force-thick-frames
 // @name            Force thick frames
-// @description     Force windows to have thick frames like in Windows Vista/7. 
+// @description     Force windows to have thick frames like in Windows Vista/7
 // @version         2
 // @author          Alcatel
 // @github          https://github.com/arukateru
@@ -9,14 +9,16 @@
 // @exclude         notepad++.exe
 // ==/WindhawkMod==
 
-// ==WindhawkModReadme== 
-/*  
-Force windows to have thick frames like in Windows Vista/7. This theme should already have thick borders (so like, not the default Windows 10 or 11 theme)
-![Screenshot](https://raw.githubusercontent.com/arukateru/ThickFramer/main/251890469-a04311e7-bbed-449f-9c92-79c642c36005.png) 
-*/ 
-// ==/WindhawkModReadme== 
+// ==WindhawkModReadme==
+/*
+Force windows to have thick frames like in Windows Vista/7. This theme should already have thick borders (so like, not the default Windows 10 or 11 theme).
+
+![Screenshot](https://raw.githubusercontent.com/arukateru/ThickFramer/main/251890469-a04311e7-bbed-449f-9c92-79c642c36005.png)
+*/
+// ==/WindhawkModReadme==
 
 using PUNICODE_STRING = PVOID;
+
 // https://github.com/sandboxie-plus/Sandboxie/blob/294966c7d6e99cd153ede87ad09aa39ef29e34c3/Sandboxie/core/dll/Win32.c#L25
 using NtUserCreateWindowEx_t =
     NTSTATUS(WINAPI*)(DWORD dwExStyle,
