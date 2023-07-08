af89717a1941fdb2743f5fc4c75fa3d30c9bbb08
Alcatel <84914212+arukateru@users.noreply.github.com>
2023-07-08 11:13:40 -0400

    Update force-thick-frames.wh.cpp

--- mods/force-thick-frames.wh.cpp
+++ mods/force-thick-frames.wh.cpp
@@ -1,6 +1,6 @@
 // ==WindhawkMod==
-// @id              force-thick-frames-clock
-// @name            Force thick frames-clock
+// @id              force-thick-frames
+// @name            Force thick frames
 // @description     Force windows to have thick frames like in Windows Vista/7. 
 // @version         2
 // @author          Alcatel
@@ -16,33 +16,32 @@
  */ 
  // ==/WindhawkModReadme== 
  
-#include <Windows.h>
+ using PUNICODE_STRING = PVOID;
 
-// Define function pointer types
-using NtUserCreateWindowEx_t = NTSTATUS(WINAPI*)(DWORD dwExStyle,
-                                                 PVOID UnsafeClassName,
-                                                 LPCWSTR VersionedClass,
-                                                 PVOID UnsafeWindowName,
-                                                 DWORD dwStyle,
-                                                 LONG x,
-                                                 LONG y,
-                                                 LONG nWidth,
-                                                 LONG nHeight,
-                                                 HWND hWndParent,
-                                                 HMENU hMenu,
-                                                 HINSTANCE hInstance,
-                                                 LPVOID lpParam,
-                                                 DWORD dwShowMode,
-                                                 DWORD dwUnknown1,
-                                                 DWORD dwUnknown2,
-                                                 PVOID qwUnknown3);
+// https://github.com/sandboxie-plus/Sandboxie/blob/294966c7d6e99cd153ede87ad09aa39ef29e34c3/Sandboxie/core/dll/Win32.c#L25
+using NtUserCreateWindowEx_t =
+    NTSTATUS(WINAPI*)(DWORD dwExStyle,
+                      PUNICODE_STRING UnsafeClassName,
+                      LPCWSTR VersionedClass,
+                      PUNICODE_STRING UnsafeWindowName,
+                      DWORD dwStyle,
+                      LONG x,
+                      LONG y,
+                      LONG nWidth,
+                      LONG nHeight,
+                      HWND hWndParent,
+                      HMENU hMenu,
+                      HINSTANCE hInstance,
+                      LPVOID lpParam,
+                      DWORD dwShowMode,
+                      DWORD dwUnknown1,
+                      DWORD dwUnknown2,
+                      VOID* qwUnknown3);
 NtUserCreateWindowEx_t NtUserCreateWindowEx_Original;
-
-// Hook function
 NTSTATUS WINAPI NtUserCreateWindowEx_Hook(DWORD dwExStyle,
-                                          PVOID UnsafeClassName,
+                                          PUNICODE_STRING UnsafeClassName,
                                           LPCWSTR VersionedClass,
-                                          PVOID UnsafeWindowName,
+                                          PUNICODE_STRING UnsafeWindowName,
                                           DWORD dwStyle,
                                           LONG x,
                                           LONG y,
@@ -55,45 +54,34 @@ NTSTATUS WINAPI NtUserCreateWindowEx_Hook(DWORD dwExStyle,
                                           DWORD dwShowMode,
                                           DWORD dwUnknown1,
                                           DWORD dwUnknown2,
-                                          PVOID qwUnknown3) {
-    // Call the original function
-    NTSTATUS result = NtUserCreateWindowEx_Original(dwExStyle, UnsafeClassName, VersionedClass,
-                                                   UnsafeWindowName, dwStyle, x, y, nWidth,
-                                                   nHeight, hWndParent, hMenu, hInstance,
-                                                   lpParam, dwShowMode, dwUnknown1, dwUnknown2,
-                                                   qwUnknown3);
-
-    // Check if the window being created is the taskbar clock flyout
-    if (VersionedClass && wcscmp(VersionedClass, L"Shell_TrayWnd") == 0) {
-        HWND hWndFlyout = FindWindowExW(hWndParent, nullptr, L"ClockFlyoutWindow", nullptr);
-        if (hWndFlyout != nullptr) {
-            // Add the border style to the flyout window
-            LONG updatedStyle = GetWindowLong(hWndFlyout, GWL_STYLE);
-            updatedStyle |= WS_THICKFRAME;
-            SetWindowLong(hWndFlyout, GWL_STYLE, updatedStyle);
-            SetWindowPos(hWndFlyout, nullptr, x, y, nWidth, nHeight, SWP_FRAMECHANGED);
-        }
+                                          VOID* qwUnknown3) {
+    if ((dwStyle & WS_CAPTION) == WS_CAPTION) {
+        dwStyle |= WS_THICKFRAME;
     }
 
-    return result;
+    return NtUserCreateWindowEx_Original(
+        dwExStyle, UnsafeClassName, VersionedClass, UnsafeWindowName, dwStyle,
+        x, y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam,
+        dwShowMode, dwUnknown1, dwUnknown2, qwUnknown3);
 }
 
 BOOL Wh_ModInit() {
+    Wh_Log(L">");
+
     HMODULE hModule = GetModuleHandle(L"win32u.dll");
     if (!hModule) {
         return FALSE;
     }
 
-    // Get the address of NtUserCreateWindowEx
     NtUserCreateWindowEx_t pNtUserCreateWindowEx =
         (NtUserCreateWindowEx_t)GetProcAddress(hModule, "NtUserCreateWindowEx");
     if (!pNtUserCreateWindowEx) {
         return FALSE;
     }
 
-    // Set the hook
-    Wh_SetFunctionHook((PVOID*)pNtUserCreateWindowEx, (PVOID*)NtUserCreateWindowEx_Hook,
-                       (PVOID**)&NtUserCreateWindowEx_Original);
+    Wh_SetFunctionHook((void*)pNtUserCreateWindowEx,
+                       (void*)NtUserCreateWindowEx_Hook,
+                       (void**)&NtUserCreateWindowEx_Original);
 
     return TRUE;
 }
