8700ddafbee73c8c13480eaf2619347877d9b367
Mark Jansen <mark.jansen@reactos.org>
2023-10-09 13:47:34 +0200

    Update PPEE/Wow64 mod to be less brittle, and also work for ResourceHacker (#340)
    Update to be less brittle, and also work for ResourceHacker

--- mods/lm-ppee-wow64.wh.cpp
+++ mods/lm-ppee-wow64.wh.cpp
@@ -1,17 +1,18 @@
 // ==WindhawkMod==
 // @id              lm-ppee-wow64
-// @name            Fix PPEE Wow64 filesystem redirection
-// @description     Disables Wow64 filesystem redirection when loading a file in PPEE
-// @version         1.0
+// @name            Fix PPEE/ResourceHacker Wow64 filesystem redirection
+// @description     Disables Wow64 filesystem redirection when loading a file in PPEE or ResourceHacker
+// @version         1.2
 // @author          Mark Jansen
 // @github          https://github.com/learn-more
 // @twitter         https://twitter.com/learn_more
 // @include         ppee.exe
+// @include         resourcehacker.exe
 // ==/WindhawkMod==
 
 // ==WindhawkModReadme==
 /*
-# Fix Wow64 filesystem redirection for PPEE
+# Fix Wow64 filesystem redirection for PPEE and ResourceHacker
 
 When loading a file from `C:\Windows\System32`, PPEE would automatically be redirected to `C:\Windows\SysWOW64`.
 
@@ -27,40 +28,22 @@ This mod disables that redirection, so that the correct file is loaded.
 */
 // ==/WindhawkModReadme==
 
-template<typename ProtoType>
-BOOL Wh_SetFunctionHookT(ProtoType targetFunction, ProtoType hookFunction, ProtoType* originalFunction)
-{
-    return Wh_SetFunctionHook((void*)targetFunction, (void*)hookFunction, (void**)originalFunction);
-}
-
-static PBYTE g_PPEE_Address;
-
-using CreateFileW_t = decltype(&CreateFileW);
-CreateFileW_t CreateFileW_Original;
-HANDLE __stdcall CreateFileW_Hook(LPCWSTR lpFileName,
-                                  DWORD dwDesiredAccess,
-                                  DWORD dwShareMode,
-                                  LPSECURITY_ATTRIBUTES lpSecurityAttributes,
-                                  DWORD dwCreationDisposition,
-                                  DWORD dwFlagsAndAttributes,
-                                  HANDLE hTemplateFile) {
-    Wh_Log(L"%s", lpFileName);
-
-    PVOID Cookie = NULL;
-    BOOL fRestore = Wow64DisableWow64FsRedirection(&Cookie);
-
-    HANDLE hFile = CreateFileW_Original(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
-
-    if (fRestore)
-        Wow64RevertWow64FsRedirection(Cookie);
-    
-    return hFile;
-}
+static BOOL g_fRevert = FALSE;
+static PVOID g_Cookie = NULL;
 
 BOOL Wh_ModInit()
 {
     Wh_Log(L"Init " WH_MOD_ID L" version " WH_MOD_VERSION);
-    g_PPEE_Address = (PBYTE)GetModuleHandleW(NULL);
-    Wh_SetFunctionHookT(CreateFileW, CreateFileW_Hook, &CreateFileW_Original);
+
+    g_fRevert = Wow64DisableWow64FsRedirection(&g_Cookie);
+    Wh_Log(L" => Disable FS redirection: %d", g_fRevert);
+
     return TRUE;
+}
+
+void Wh_ModUninit(void)
+{
+    Wh_Log(L"Uninit " WH_MOD_ID );
+    if (g_fRevert)
+        Wow64RevertWow64FsRedirection(g_Cookie);
 }
