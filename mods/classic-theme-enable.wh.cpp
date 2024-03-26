7d8aa239ca01b4edc7ec8ca81f6aea4fc8ddc439
Anixx <neptunia@mail.ru>
2024-03-26 15:35:25 +0200

    Update classic-theme-enable.wh.cpp (#577)
    Update readme

--- mods/classic-theme-enable.wh.cpp
+++ mods/classic-theme-enable.wh.cpp
@@ -2,7 +2,7 @@
 // @id              classic-theme-enable
 // @name            Enable Classic Theme by handle method
 // @description     Disables theming by closing the handle
-// @version         1.0.1
+// @version         1.0.2
 // @author          Anixx
 // @github 			https://github.com/Anixx
 // @include         winlogon.exe
@@ -21,16 +21,15 @@ The mod injects only in the process Winlogon.exe, and exits once
 the handle of the memory area is closed. It does not hook
 any functions. 
 
-Make sure that the option allowing injecting into critical system processes
-is enabled in Windhawk options.
-
-The mod affects only windows that are created after enabling the mod.
+The mod affects only programs started after enabling the mod.
 
 Windows Classic theme is not compatible with Windows 10 Task Manager.
 Before enabling the mode, install the [classic task manager](https://winaero.com/get-classic-old-task-manager-in-windows-10/).
 
-Enabling the mod will make UWP apps unfunctional. Install a mod that fixes
-this, for instance, [this one](https://winclassic.boards.net/thread/2058/classic-uwp-fix)
+Enabling the mod will make UWP apps to hang. Install the *Classic UWP Fix* mod to mitigate this.
+
+Some windows will manifest unwanted transparency. 
+To fix this, install the *Classic theme transparency fix* mod.
 
 Windows Classic theme is not compatible with Windows 11 and Windows 10 taskbars
 out of the box. It is recommended to use [Explorer Patcher](https://github.com/valinet/ExplorerPatcher) 
