7b54130e20365428923b5a94545a634680a161b0
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2024-05-13 01:24:58 +0300

    Implemented some of the checks from #104

--- .github/pr_validation.py
+++ .github/pr_validation.py
@@ -1,71 +1,156 @@
 '''
-PROJECT:     PR Validation for WindHawk
+PROJECT:     PR Validation for Windhawk
 LICENSE:     MIT (https://spdx.org/licenses/MIT)
 PURPOSE:     Verifies the mod information in the modified mods.
 COPYRIGHT:   Copyright 2023 Mark Jansen <mark.jansen@reactos.org>
 '''
-from optparse import OptionParser
-from pathlib import Path
+import os
+import re
 import sys
+from pathlib import Path
+from typing import Optional, Tuple
+
+DISALLOWED_AUTHORS = []
 
 
 def add_warning(file: Path, line: int, message: str):
-    print(f'::warning file={file!s},line={line}::{message}')
+    print(f'::warning file={file},line={line}::{message}')
     return 1
 
 
-def parse_file(file: Path, expected_author):
+def parse_file(file: Path, expected_author: str):
     warnings = 0
-    print(f'Checking {file=!s}')
+    print(f'Checking {file=}')
+
+    core_properties: dict[str, Optional[Tuple[str, int]]] = {
+        'id': None,
+        'github': None,
+        'version': None,
+        'compilerOptions': None,
+    }
+
+    all_properties: set[str] = set()
+
     for idx, line in enumerate(file.read_text().splitlines()):
-        if not line.startswith('//'):
+        if idx == 0:
+            if not re.fullmatch(r'//[ \t]+==WindhawkMod==[ \t]*', line):
+                warnings += add_warning(file, idx + 1,
+                                        'First line must be "// ==WindhawkMod=="')
+                break
             continue
-        line = line[2:].strip()
-        # Validate the github id specified against the PR author
-        if line.startswith('@github'):
-            line = line[7:].strip()
-            if line != f'https://github.com/{expected_author}':
-                warnings += add_warning(file, idx + 1,
-                                        f'Expected the author to be "{expected_author}"')
-        # Validate the mod ID against the filename
-        if line.startswith('@id'):
-            line = line[3:].strip()
-            expected = file.with_suffix('').with_suffix(
-                '').name    # Strip the two extensions
-            if line != expected:
-                warnings += add_warning(file, idx + 1,
-                                        f'Expected the id to be "{expected}"')
+
+        if re.fullmatch(r'//[ \t]+==\/WindhawkMod==[ \t]*', line):
+            break
+
+        if line.strip() == '':
+            continue
+
+        match = re.fullmatch(
+            r'//[ \t]+@([a-zA-Z]+)(?::([a-z]{2}(?:-[A-Z]{2})?))?[ \t]+(.*)', line.strip())
+        if not match:
+            warnings += add_warning(file, idx + 1,
+                                    'Invalid metadata line format')
+            continue
+
+        key = match.group(1)
+        language = match.group(2)
+        value = match.group(3)
+
+        all_properties.add(key)
+
+        if key not in core_properties:
+            continue
+
+        if language is not None:
+            warnings += add_warning(file, idx + 1,
+                                    'Language cannot be specified for this property')
+            continue
+
+        if core_properties[key] is not None:
+            warnings += add_warning(file, idx + 1,
+                                    f'{key} must be specified only once')
+            continue
+
+        core_properties[key] = value, idx
+
+    # Validate the github id specified against the PR author
+    if core_properties['github'] is not None:
+        value, idx = core_properties['github']
+        if value != f'https://github.com/{expected_author}':
+            warnings += add_warning(file, idx + 1,
+                                    f'Expected the author to be "{expected_author}"')
+    else:
+        warnings += add_warning(file, 1, 'Missing @github')
+
+    # Validate the mod ID against the filename
+    if core_properties['id'] is not None:
+        value, idx = core_properties['id']
+        # Strip the two extensions
+        expected = file.stem
+        if value != expected:
+            warnings += add_warning(file, idx + 1,
+                                    f'Expected the id to be "{expected}"')
+    else:
+        warnings += add_warning(file, 1, 'Missing @id')
+
+    # Validate the version
+    if core_properties['version'] is not None:
+        value, idx = core_properties['version']
+        if not re.fullmatch(r'([0-9]+\.)*[0-9]+', value):
+            warnings += add_warning(file, idx + 1,
+                                    'Version must contain only numbers and dots')
+    else:
+        warnings += add_warning(file, 1, 'Missing @version')
+
+    if core_properties['compilerOptions'] is not None:
+        value, idx = core_properties['compilerOptions']
+        if not re.fullmatch(r'((-[lD]\S+|-Wl,--export-all-symbols)\s+)+', value + ' '):
+            warnings += add_warning(file, idx + 1,
+                                    'Compiler options require manual verification')
+
+    if 'author' not in all_properties:
+        warnings += add_warning(file, 1, 'Missing @author')
 
     # Validate that this file has the required extensions
     if ''.join(file.suffixes) != '.wh.cpp':
         warnings += add_warning(file, 1, 'Filename should end with .wh.cpp')
 
+    # Validate file name
+    if not re.fullmatch(r'([0-9a-z]+-)*[0-9a-z]+', file.with_suffix('').with_suffix('').name):
+        warnings += add_warning(file, 1,
+                                'Filename must contain only letters, numbers and dashes')
+
+    # Validate file path
+    if file.parent != Path('mods'):
+        warnings += add_warning(file, 1,
+                                'File is not placed in the mods folder')
+
     return warnings
 
 
 def main():
-    parser = OptionParser()
-    parser.add_option("--author", help="Specify the PR author")
-    (options, args) = parser.parse_args()
-    if not options.author:
-        parser.error("Please specify the PR author")
+    pr_author = os.environ['PR_AUTHOR']
+    if pr_author in DISALLOWED_AUTHORS:
+        sys.exit(f'Submissions from {pr_author} are not allowed')
 
     warnings = 0
-    for idx, path in enumerate([Path(filename) for filename in args]):
-        if not path or len(path.parts) == 0:
-            # Not sure if this could ever happen, but we should not crash
-            warnings += add_warning(Path(__file__), 1,
-                                    f'Unable to parse arguments: {args}')
-            continue
-        # We expect relative paths from the repo root
-        if path.parts[0] != 'mods':
-            warnings += add_warning(path, 1,
-                                    'File is not placed in the mods folder')
+
+    paths = [Path(p)
+             for p in os.environ['ALL_CHANGED_AND_MODIFIED_FILES'].splitlines()]
+
+    added_count = int(os.environ['ADDED_FILES_COUNT'])
+    modified_count = int(os.environ['MODIFIED_FILES_COUNT'])
+    all_count = int(os.environ['ALL_CHANGED_AND_MODIFIED_FILES_COUNT'])
+
+    if (added_count, modified_count, all_count) not in [(1, 0, 1), (0, 1, 1)]:
+        warnings += add_warning(paths[0], 1, f'Must be one added or one modified file, got '
+                                f'{added_count=} {modified_count=} {all_count=}')
+
+    for path in paths:
         # Validate everything as if it were a mod, if it has the .cpp extension
-        if path.name.endswith('.cpp'):
-            warnings += parse_file(path, options.author)
-        if idx > 0:
-            warnings += add_warning(path, 1, 'More than one file was changed')
+        if path.suffix == '.cpp':
+            warnings += parse_file(path, pr_author)
+
     if warnings > 0:
         sys.exit(f'Got {warnings} warnings, please inspect the PR')
 
