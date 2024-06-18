bb1116c9071723741b6b7bbab0e511282c2aa4f0
Anixx <neptunia@mail.ru>
2024-06-18 20:16:31 +0300

    Update classic-browser-fix.wh.cpp (#736)
    add Skype

--- mods/classic-browser-fix.wh.cpp
+++ mods/classic-browser-fix.wh.cpp
@@ -2,12 +2,13 @@
 // @id              classic-browser-fix
 // @name            Fix browsers for Windows Classic theme
 // @description     Forces the border from Aero Light theme to fix glitched border in Classic theme
-// @version         1.0
+// @version         1.1
 // @author 			Anixx
 // @github 			https://github.com/Anixx
 // @include         msedge.exe
 // @include         chrome.exe
 // @include         chromium.exe
+// @include         Skype.exe
 // @include         steam.exe
 
 // @compilerOptions -luxtheme
@@ -15,7 +16,8 @@
 
 // ==WindhawkModReadme==
 /*
-This mod forces Aero Lite window borders on browsers MS Edge, Chrome, Chromium and Steam app,
+This mod forces Aero Lite window borders on Chromium-based browsers and applications
+MS Edge, Chrome, Chromium, Skype, and Steam app,
 which fixes their glitches when using Windows Classic theme.
 
 ![Aero Lite](https://i.imgur.com/5enqSD8.png)
