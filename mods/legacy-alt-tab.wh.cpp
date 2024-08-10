6120b0fafd41ce4a235e980f1a4bab4f5083bbfc
Anixx <neptunia@mail.ru>
2024-08-10 16:24:00 +0300

    Create legacy-alt-tab.wh.cpp

---
+++ mods/legacy-alt-tab.wh.cpp
@@ -0,0 +1,49 @@
+// ==WindhawkMod==
+// @id           legacy-alt-tab
+// @name         Legacy Alt+Tab dialog
+// @description  Enables legacy Alt+Tab dialog from Windows XP and Windows 2000
+// @version      1.0
+// @author       Anixx
+// @github       https://github.com/Anixx
+// @include      explorer.exe
+// ==/WindhawkMod==
+
+// ==WindhawkModReadme==
+/*
+Enables legacy Alt+Tab dialog from Windows XP and Windows 2000
+*/
+// ==/WindhawkModReadme==
+
+#include <windows.h>
+
+typedef LONG (WINAPI *REGQUERYVALUEEXW)(HKEY hKey, LPCWSTR lpValueName, LPDWORD lpReserved, LPDWORD lpType, LPBYTE lpData, LPDWORD lpcbData);
+
+REGQUERYVALUEEXW pOriginalRegQueryValueExW;
+
+LONG WINAPI RegQueryValueExWHook(HKEY hKey, LPCWSTR lpValueName, LPDWORD lpReserved, LPDWORD lpType, LPBYTE lpData, LPDWORD lpcbData)
+{   
+
+    if (lstrcmpiW(lpValueName, L"AltTabSettings") == 0)
+        
+    { 
+            if (lpType)
+                *lpType = REG_DWORD;
+            if (lpData && lpcbData && *lpcbData >= sizeof(DWORD))
+            {
+                *(DWORD*)lpData = 1;
+                *lpcbData = sizeof(DWORD);
+            }
+            return ERROR_SUCCESS;
+    }
+
+    return pOriginalRegQueryValueExW(hKey, lpValueName, lpReserved, lpType, lpData, lpcbData);
+}
+
+BOOL Wh_ModInit(void)
+{
+    Wh_Log(L"Init");
+
+    Wh_SetFunctionHook((void*)GetProcAddress(LoadLibrary(L"kernelbase.dll"), "RegQueryValueExW"), (void*)RegQueryValueExWHook, (void**)&pOriginalRegQueryValueExW);
+
+    return TRUE;
+}
