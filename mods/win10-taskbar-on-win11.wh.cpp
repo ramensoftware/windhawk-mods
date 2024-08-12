415fbf01bb4793e72a0408ac9bde0f3fad5353f7
Anixx <neptunia@mail.ru>
2024-08-12 17:10:59 +0300

    Update win10-taskbar-on-win11.wh.cpp (#852)
    More clarity in description

--- mods/win10-taskbar-on-win11.wh.cpp
+++ mods/win10-taskbar-on-win11.wh.cpp
@@ -1,8 +1,8 @@
 // ==WindhawkMod==
 // @id           win10-taskbar-on-win11
 // @name         Windows 10 taskbar on Windows 11
-// @description  Enables Windows 10 taskbar on Windows 11 versions 21H2 to 23H2 via registry
-// @version      1.0
+// @description  Enables Windows 10 taskbar on Windows 11 versions 21H2 to 23H2
+// @version      1.0.1
 // @author       Anixx
 // @github       https://github.com/Anixx
 // @include      explorer.exe
@@ -10,15 +10,15 @@
 
 // ==WindhawkModReadme==
 /*
-This mod enables Windows 10 taskbar on Windows 11 versions 21H2 to 23H2 via registry method, without
-using Explorer Patcher. 
+This mod enables Windows 10 taskbar on Windows 11 versions 21H2 to 23H2 via registry query hook, without
+using Explorer Patcher. The actual registry values remain unaffected.
 
 It may be useful also on later versions of Windows with Explorer.exe downgraded to previous versions
 and/or manipulated Windows features using vivetool utility.
 
 If you want to get rid of the cogwheel icon in the tray area, use the mod "Hide Action Center icon".
 You can restore the clock and set up the tray icons using legacy tray setup dialog,
-which you can start with command `shell:::{05d7b0f4-2121-4eff-bf6b-ed3f69b894d9}`
+which you can start with the command `explorer shell:::{05d7b0f4-2121-4eff-bf6b-ed3f69b894d9}`.
 */
 // ==/WindhawkModReadme==
 
