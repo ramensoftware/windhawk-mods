f9824e00f4322c21ce7731c10d8a57480fbdaa0e
isabella <kawapure@gmail.com>
2024-10-25 07:09:51 -0700

    Fix Qt scrollbar grippers 1.1: Fix control detection (#1140)
    * Fix control detection. \
    Previously, we attempted to draw the gripper overlay for any control that existed, not just scrollbars. This is because the control theme opened by the HTHEME was not checked. Now, we only try drawing scrollbar grippers if we know the control itself is a scrollbar.

--- mods/fix-qt-scrollbar-grippers.wh.cpp
+++ mods/fix-qt-scrollbar-grippers.wh.cpp
@@ -2,7 +2,7 @@
 // @id              fix-qt-scrollbar-grippers
 // @name            Fix Qt scrollbar grippers
 // @description     Fixes scrollbar grippers in Qt 6.1+
-// @version         1.0
+// @version         1.1
 // @author          kawapure
 // @github          https://github.com/kawapure
 // @twitter         https://twitter.com/kawaipure
@@ -32,7 +32,8 @@ for injection.
 
 In Windhawk, go to the "Advanced" tab and scroll down to 
 "Custom process inclusion list". In that box, put the filename of the `.exe`. 
-The mod will immediately apply to those programs after you click "Save". 
+The mod will immediately apply to those programs after you click "Save", although the best results usually require a restart
+of that program because Qt may cache the bitmap, causing the element to sometimes appear unmodified.
 */
 // ==/WindhawkModReadme==
 
@@ -40,6 +41,10 @@ The mod will immediately apply to those programs after you click "Save".
 #include <windhawk_utils.h>
 #include <uxtheme.h>
 #include <vsstyle.h>
+#include <winerror.h>
+
+// Undocumented export from UXTheme to get the class from a theme handle.
+HRESULT STDMETHODCALLTYPE (*GetThemeClass)(HTHEME hTheme, LPCWSTR pszClassIdList, int cchClass);
 
 using DrawThemeBackgroundEx_t = decltype(&DrawThemeBackgroundEx);
 DrawThemeBackgroundEx_t DrawThemeBackgroundEx_orig;
@@ -47,16 +52,25 @@ HRESULT WINAPI DrawThemeBackgroundEx_hook(HTHEME hTheme, HDC hdc, int iPartId, i
 {
     HRESULT result = DrawThemeBackgroundEx_orig(hTheme, hdc, iPartId, iStateId, pRect, pOptions);
 
-    if (iPartId == SBP_THUMBBTNHORZ || iPartId == SBP_THUMBBTNVERT)
+    WCHAR szClassName[512];
+    HRESULT hrGetString = GetThemeClass(hTheme, szClassName, 512);
+
+    if (
+        SUCCEEDED(hrGetString) &&
+        wcsicmp(szClassName, L"ScrollBar") == 0 &&
+        (iPartId == SBP_THUMBBTNHORZ || iPartId == SBP_THUMBBTNVERT)
+    )
     {
         if (iPartId == SBP_THUMBBTNHORZ)
             Wh_Log(L"dtb: is SBP_THUMBBTNHORZ");
         else if (iPartId == SBP_THUMBBTNVERT)
             Wh_Log(L"dtb: is SBP_THUMBBTNVERT");
 
+        Wh_Log(L"iStateId: %d", iStateId);
+
         int gripperPart = iPartId == SBP_THUMBBTNHORZ
-                ? SBP_GRIPPERHORZ
-                : SBP_GRIPPERVERT;
+            ? SBP_GRIPPERHORZ
+            : SBP_GRIPPERVERT;
 
         SIZE gripperSize = { 0 };
         GetThemePartSize(hTheme, hdc, gripperPart, iStateId, nullptr, TS_TRUE, &gripperSize);
@@ -67,6 +81,7 @@ HRESULT WINAPI DrawThemeBackgroundEx_hook(HTHEME hTheme, HDC hdc, int iPartId, i
         )
         {
             Wh_Log(L"drawing gripper");
+            Wh_Log(L"is dimensions %dx%d", pRect->right - pRect->left, pRect->bottom - pRect->top);
 
             DrawThemeBackground(hTheme, hdc, gripperPart, iStateId, pRect, nullptr);
         }
@@ -81,11 +96,35 @@ BOOL Wh_ModInit()
 {
     Wh_Log(L"Init");
 
-    Wh_SetFunctionHook(
+    HMODULE hModUxtheme = GetModuleHandleW(L"uxtheme.dll");
+
+    if (!hModUxtheme)
+    {
+        Wh_Log(L"UXTheme module not loaded. Mod failed to load.");
+        return FALSE;
+    }
+
+    // UXTheme ordinal export #74 = GetThemeClass: Gets the class name opened by that theme handle.
+    *(void **)&GetThemeClass = (void *)GetProcAddress(hModUxtheme, MAKEINTRESOURCEA(74));
+
+    if (!GetThemeClass)
+    {
+        Wh_Log(
+            L"GetThemeClass not exported by UXTheme. The mod may not be compatible with "
+            L"your version of Windows."
+        );
+        return FALSE;
+    }
+
+    if (!Wh_SetFunctionHook(
         (void *)DrawThemeBackgroundEx,
         (void *)DrawThemeBackgroundEx_hook,
         (void **)&DrawThemeBackgroundEx_orig
-    );
+    ))
+    {
+        Wh_Log(L"Failed to install DrawThemeBackgroundEx hooks. Mod failed to load.");
+        return FALSE;
+    }
 
     return TRUE;
 }
