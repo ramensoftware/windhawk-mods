facac5ede89fabd7393cf576ae0d6c270504969c
aubrey <44238627+aubymori@users.noreply.github.com>
2024-11-10 05:23:48 -0600

    Custom Shutdown Dialog 1.1.0 (#1226)
    - Add ability to override logoff dialog (used in Windows XP Explorer)

--- mods/custom-shutdown-dialog.wh.cpp
+++ mods/custom-shutdown-dialog.wh.cpp
@@ -2,7 +2,7 @@
 // @id              custom-shutdown-dialog
 // @name            Custom Shutdown Dialog
 // @description     Override the classic shutdown dialog in Explorer with your own
-// @version         1.0.1
+// @version         1.1.0
 // @author          aubymori
 // @github          https://github.com/aubymori
 // @include         explorer.exe
@@ -14,23 +14,34 @@
 # Custom Shutdown Dialog
 Override the classic shutdown dialog in Explorer
 which is invoked with `ALT`+`F4` with your own program.
+
+This also lets you override the action of the "Log Off" button
+in Windows XP's Explorer, which on Windows 10 normally instantly logs
+the user out.
 */
 // ==/WindhawkModReadme==
 
 // ==WindhawkModSettings==
 /*
 - exe: C:\Classic\ClassicShutdown\ClassicShutdown.exe
-  $name: Executable
-  $description: Path to executable to open instead of dialog.
+  $name: Shutdown executable
+  $description: Path to executable to open instead of shutdown dialog.
 - args: /style classic
-  $name: Arguments
-  $description: Arguments to pass to the executable, if any.
+  $name: Shutdown arguments
+  $description: Arguments to pass to the shutdown executable, if any.
+- logoffexe: C:\Classic\ClassicShutdown\ClassicShutdown.exe
+  $name: Logoff executable
+  $description: Path to executable to open instead of logoff dialog.
+- logoffargs: /logoff /style classic
+  $name: Logoff arguments
+  $description: Arguments to pass to the logoff executable, if any.
 */
 // ==/WindhawkModSettings==
 
 #include <windhawk_utils.h>
 
-WindhawkUtils::StringSetting g_szExe, g_szArgs;
+WindhawkUtils::StringSetting g_szShutdownExe, g_szShutdownArgs;
+WindhawkUtils::StringSetting g_szLogoffExe, g_szLogoffArgs;
 
 __int64 (*_ShutdownDialogEx_orig)(HWND, int, int, UINT);
 __int64 _ShutdownDialogEx_hook(
@@ -43,12 +54,25 @@ __int64 _ShutdownDialogEx_hook(
     ShellExecuteW(
         hWndParent,
         L"open",
-        g_szExe,
-        g_szArgs,
+        g_szShutdownExe,
+        g_szShutdownArgs,
         NULL,
         SW_NORMAL
     );
     return 0;
+}
+
+void (*LogoffWindowsDialog_orig)(HWND);
+void LogoffWindowsDialog_hook(HWND hWndParent)
+{
+    ShellExecuteW(
+        hWndParent,
+        L"open",
+        g_szLogoffExe,
+        g_szLogoffArgs,
+        NULL,
+        SW_NORMAL
+    );
 }
 
 WindhawkUtils::SYMBOL_HOOK shutdownuxDllHooks[] = {
@@ -60,12 +84,25 @@ WindhawkUtils::SYMBOL_HOOK shutdownuxDllHooks[] = {
         _ShutdownDialogEx_hook,
         false
     }
+};\
+
+WindhawkUtils::SYMBOL_HOOK shell32DllHooks[] = {
+    {
+        {
+            L"LogoffWindowsDialog"
+        },
+        &LogoffWindowsDialog_orig,
+        LogoffWindowsDialog_hook,
+        false
+    }
 };
 
 void LoadSettings(void)
 {
-    g_szExe = WindhawkUtils::StringSetting::make(L"exe");
-    g_szArgs = WindhawkUtils::StringSetting::make(L"args");
+    g_szShutdownExe = WindhawkUtils::StringSetting::make(L"exe");
+    g_szShutdownArgs = WindhawkUtils::StringSetting::make(L"args");
+    g_szLogoffExe = WindhawkUtils::StringSetting::make(L"logoffexe");
+    g_szLogoffArgs = WindhawkUtils::StringSetting::make(L"logoffargs");
 }
 
 BOOL Wh_ModInit(void)
@@ -86,6 +123,23 @@ BOOL Wh_ModInit(void)
     ))
     {
         Wh_Log(L"Failed to hook _ShutdownDialogEx");
+        return FALSE;
+    }
+
+    HMODULE hShell32 = LoadLibraryW(L"shell32.dll");
+    if (!hShell32)
+    {
+        Wh_Log(L"Failed to load shell32.dll");
+        return FALSE;
+    }
+
+    if (!WindhawkUtils::HookSymbols(
+        hShell32,
+        shell32DllHooks,
+        ARRAYSIZE(shell32DllHooks)
+    ))
+    {
+        Wh_Log(L"Failed to hook LogoffWindowsDialog");
         return FALSE;
     }
 
