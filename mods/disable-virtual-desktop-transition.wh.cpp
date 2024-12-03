f5395817e52442e470b1ba4df525fe75c4bea1d4
isabella lulamoon <kawapure@gmail.com>
2024-12-03 10:59:42 -0700

    Disable Virtual Desktop Transition: Revamp readme and limit architecture
    Signed-off-by: Isabella Lulamoon <kawapure@gmail.com>

--- mods/disable-virtual-desktop-transition.wh.cpp
+++ mods/disable-virtual-desktop-transition.wh.cpp
@@ -8,6 +8,7 @@
 // @twitter         https://twitter.com/kawaipure
 // @homepage        https://kawapure.github.io
 // @include         dwm.exe
+// @architecture    x86-64
 // @compilerOptions -lkernel32
 // ==/WindhawkMod==
 
@@ -17,8 +18,13 @@
 
 This mod disables the animation seen when switching between virtual desktops in Windows 10.
 
-In order to use this mod, you have to hook into `dwm.exe`. In order to do this, you must go into Windhawk advanced settings
-and remove the exclusion entry for `<critical_system_processes>`.
+## ⚠ Important usage note ⚠ 
+  
+In order to use this mod, you must allow Windhawk to inject into the **dwm.exe** 
+system process. To do so, add it to the process inclusion list in the advanced 
+settings. If you do not do this, it will silently fail to inject.
+  
+![Advanced settings screenshot](https://i.imgur.com/LRhREtJ.png) 
 */
 // ==/WindhawkModReadme==
 
