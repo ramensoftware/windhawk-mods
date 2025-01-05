f4ce9a5aabbc3e4788554e88d7ddb83516aacd2b
Mgg Sk <mggskmail@gmail.com>
2025-01-05 17:05:45 +0100

    Dark mode context menus 1.1 (#1406)
    Added a option to only use context menus in dark mode if Windows is in dark mode. Also cleaned up the code.

--- mods/dark-menus.wh.cpp
+++ mods/dark-menus.wh.cpp
@@ -2,7 +2,7 @@
 // @id              dark-menus
 // @name            Dark mode context menus
 // @description     Enables dark mode for all win32 menus.
-// @version         1.0
+// @version         1.1
 // @author          Mgg Sk
 // @github          https://github.com/MGGSK
 // @include         *
@@ -21,7 +21,19 @@ Forces dark mode for all win32 context menus to create a more consistent UI.
 */
 // ==/WindhawkModReadme==
 
+// ==WindhawkModSettings==
+/*
+- AppMode: ForceDark
+  $name: When do you want context menus in dark mode?
+  $description: Set to "Only when Windows is in dark mode" if you switch between light and dark mode a lot or are using a tool like Auto Dark Mode.
+  $options:
+  - ForceDark: Always
+  - AllowDark: Only when Windows is in dark mode.
+*/
+// ==/WindhawkModSettings==
+
 #include <minwindef.h>
+#include <windhawk_api.h>
 #include <winerror.h>
 
 enum AppMode
@@ -40,10 +52,32 @@ FlushMenuThemes_T FlushMenuThemes;
 SetPreferredAppMode_T SetPreferredAppMode;
 
 //Applies the theme to all menus.
-HRESULT ApplyTheme()
+HRESULT ApplyTheme(AppMode inputTheme = Max)
 {
+    AppMode theme = inputTheme;
+
+    //Get the saved theme from the settings.
+    if(theme == Max)
+    {
+        PCWSTR savedTheme = Wh_GetStringSetting(L"AppMode");
+
+        if(wcscmp(savedTheme, L"AllowDark") == 0)
+            theme = AllowDark;
+        else
+            theme = ForceDark;
+
+        Wh_FreeStringSetting(savedTheme);
+    }
+
+    //Apply the theme
     FlushMenuThemes();
-    return SetPreferredAppMode(ForceDark);
+    return SetPreferredAppMode(theme);
+}
+
+//Updates the theme of the window when the settings change.
+void Wh_ModSettingsChanged()
+{
+    ApplyTheme();
 }
 
 //Import functions
@@ -62,6 +96,5 @@ BOOL Wh_ModInit() {
 //Restores the default theme.
 void Wh_ModUninit()
 {
-    FlushMenuThemes();
-    SetPreferredAppMode(Default);
+    ApplyTheme(Default);
 }
