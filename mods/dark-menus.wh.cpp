5aa092314b6b2d6681104b319a5563f8785aec83
Mgg Sk <mggskmail@gmail.com>
2025-01-20 20:57:07 +0100

    Added a compatibility check to avoid crashes on incompatible Windows versions. The readme now shows Windows 10 screenshots. Code cleanup (#1450)
    Added a compatibility check to avoid crashes on incompatible Windows versions.
    The readme now shows Windows 10 screenshots.
    Code cleanup

--- mods/dark-menus.wh.cpp
+++ mods/dark-menus.wh.cpp
@@ -2,7 +2,7 @@
 // @id              dark-menus
 // @name            Dark mode context menus
 // @description     Enables dark mode for all win32 menus.
-// @version         1.1
+// @version         1.2
 // @author          Mgg Sk
 // @github          https://github.com/MGGSK
 // @include         *
@@ -11,13 +11,15 @@
 // ==WindhawkModReadme==
 /*
 # Dark mode context menus
-Forces dark mode for all win32 context menus to create a more consistent UI.
+Forces dark mode for all win32 context menus to create a more consistent UI. Requires Windows 10 build 18362 or later.
 
 ### Before:
 ![Before](https://i.imgur.com/bGRVJz8.png)
+![Before10](https://i.imgur.com/FGyph05.png)
 
 ### After:
 ![After](https://i.imgur.com/BURKEki.png)
+![After10](https://i.imgur.com/MUqVAcG.png)
 */
 // ==/WindhawkModReadme==
 
@@ -28,13 +30,15 @@ Forces dark mode for all win32 context menus to create a more consistent UI.
   $description: Set to "Only when Windows is in dark mode" if you switch between light and dark mode a lot or are using a tool like Auto Dark Mode.
   $options:
   - ForceDark: Always
-  - AllowDark: Only when Windows is in dark mode.
+  - AllowDark: Only when Windows is in dark mode
 */
 // ==/WindhawkModSettings==
 
-#include <minwindef.h>
 #include <windhawk_api.h>
-#include <winerror.h>
+#include <windows.h>
+
+using RtlGetNtVersionNumbers_T = void (WINAPI *)(LPDWORD, LPDWORD, LPDWORD);
+RtlGetNtVersionNumbers_T RtlGetNtVersionNumbers;
 
 enum AppMode
 {
@@ -46,13 +50,13 @@ enum AppMode
 };
 
 using FlushMenuThemes_T = void (WINAPI *)();
-using SetPreferredAppMode_T = HRESULT (WINAPI *)(AppMode appMode);
+using SetPreferredAppMode_T = HRESULT (WINAPI *)(AppMode);
 
 FlushMenuThemes_T FlushMenuThemes;
 SetPreferredAppMode_T SetPreferredAppMode;
 
 //Applies the theme to all menus.
-HRESULT ApplyTheme(AppMode inputTheme = Max)
+HRESULT ApplyTheme(const AppMode inputTheme = Max)
 {
     AppMode theme = inputTheme;
 
@@ -74,14 +78,40 @@ HRESULT ApplyTheme(AppMode inputTheme = Max)
     return SetPreferredAppMode(theme);
 }
 
+//Checks if the windows build is 18362 or later.
+bool IsAPISupported()
+{
+    if(!RtlGetNtVersionNumbers)
+    {
+        HMODULE hNtDll = LoadLibraryExW(L"ntdll.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+        FARPROC pRtlGetNtVersionNumbers = GetProcAddress(hNtDll, "RtlGetNtVersionNumbers");
+	    RtlGetNtVersionNumbers = reinterpret_cast<RtlGetNtVersionNumbers_T>(pRtlGetNtVersionNumbers);
+    }
+
+    DWORD build;
+    RtlGetNtVersionNumbers(nullptr, nullptr, &build);
+
+    build &= ~0xF0000000;
+    return build >= 18362;
+}
+
 //Updates the theme of the window when the settings change.
 void Wh_ModSettingsChanged()
 {
+    Wh_Log(L"Settings changed");
     ApplyTheme();
 }
 
 //Import functions
 BOOL Wh_ModInit() {
+    if(!IsAPISupported())
+    {
+        Wh_Log(L"Outdated Windows version!");
+        return FALSE;
+    }
+
+    Wh_Log(L"Init");
+
     HMODULE hUxtheme = LoadLibraryExW(L"uxtheme.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
     FARPROC pSetPreferredAppMode = GetProcAddress(hUxtheme, MAKEINTRESOURCEA(135));
     FARPROC pFlushMenuThemes = GetProcAddress(hUxtheme, MAKEINTRESOURCEA(136));
@@ -96,5 +126,6 @@ BOOL Wh_ModInit() {
 //Restores the default theme.
 void Wh_ModUninit()
 {
+    Wh_Log(L"Restoring the default theme.");
     ApplyTheme(Default);
 }
