430f8fff022ac96d7fbe7a502dc40a9ffc63e127
Anixx <neptunia@mail.ru>
2025-02-15 14:21:18 +0200

    Update icon16bitfix.wh.cpp (#1541)
    Add license tag according to the license on the original code.

--- mods/icon16bitfix.wh.cpp
+++ mods/icon16bitfix.wh.cpp
@@ -2,7 +2,7 @@
 // @id              icon16bitfix
 // @name            Icons of Win16 apps in Explorer
 // @description     Adds support for icons of 16-bit (Win16) applications in File Explorer
-// @version         1.0.1
+// @version         1.0.2
 // @author          anixx
 // @github          https://github.com/Anixx
 // @include         explorer.exe
@@ -15,6 +15,7 @@ The mod is adapted from [Icon16bitFix utility](https://github.com/otya128/Icon16
 
 ![Foler view](https://i.imgur.com/g4epNjk.png)
 
+License: LGPL-2.1 or later
 */
 // ==/WindhawkModReadme==
 
