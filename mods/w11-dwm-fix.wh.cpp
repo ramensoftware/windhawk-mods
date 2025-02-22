e9d3e509ca33640fa2b5025fde5991fe5dc1c154
Tech Stuff <github@teknixstuff.com>
2025-02-22 15:52:46 +0000

    Update w11-dwm-fix.wh.cpp

--- mods/w11-dwm-fix.wh.cpp
+++ mods/w11-dwm-fix.wh.cpp
@@ -21,67 +21,6 @@ Based on Ittr's uDWM patches.
 
 #include <windhawk_utils.h>
 
-/*
-#include <vector>
-#define REL(addr, offset) ((addr + offset + 4) + *(int32_t*)(addr + offset))
-#define OFF(addr, offset) addr = addr + offset;
-
-static uintptr_t FindPattern(const char* signature, uintptr_t base_address, int Relative = 0, int Offset = 0)
-{
-    static auto patternToByte = [](const char* pattern)
-    {
-        auto bytes = std::vector<int>{};
-        const auto start = const_cast<char*>(pattern);
-        const auto end = const_cast<char*>(pattern) + strlen(pattern);
-        for (auto current = start; current < end; ++current)
-        {
-            if (*current == '?')
-            {
-                ++current;
-                if (*current == '?')
-                    ++current;
-                bytes.push_back(-1);
-            }
-            else { bytes.push_back(strtoul(current, &current, 16)); }
-        }
-        return bytes;
-    };
-    const auto dosHeader = (PIMAGE_DOS_HEADER)base_address;
-    const auto ntHeaders = (PIMAGE_NT_HEADERS)((std::uint8_t*)base_address + dosHeader->e_lfanew);
-    const auto sizeOfImage = ntHeaders->OptionalHeader.SizeOfImage;
-    auto patternBytes = patternToByte(signature);
-    const auto scanBytes = reinterpret_cast<std::uint8_t*>(base_address);
-    const auto s = patternBytes.size();
-    const auto d = patternBytes.data();
-    for (auto i = 0ul; i < sizeOfImage - s; ++i)
-    {
-        bool found = true;
-        for (auto j = 0ul; j < s; ++j)
-        {
-            if (scanBytes[i + j] != d[j] && d[j] != -1)
-            {
-                found = false;
-                break;
-            }
-        }
-        if (found)
-        {
-            uintptr_t address = reinterpret_cast<uintptr_t>(&scanBytes[i]);
-            if (Relative)
-            {
-                REL(address, Relative);
-            }
-            if (Offset)
-            {
-                OFF(address, Offset);
-            }
-            return address;
-        }
-    }
-    return NULL;
-}
-*/
-
 char* s_pDesktopManagerInstance;
 
 bool g_inCTopLevelWindow_UpdateWindowVisuals = false;
