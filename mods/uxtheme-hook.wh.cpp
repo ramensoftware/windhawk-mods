73559eaa7813b37b0a91b0a85fd03007aa371cea
Rounak <70931017+rounk-ctrl@users.noreply.github.com>
2025-04-23 12:11:24 +0400

    UXTheme Hook 1.3 (#1815)
    - Prevent launching settings on theme apply
    - Fix SetSysColors hook, by getting the value from registry
    - Update FAQ

--- mods/uxtheme-hook.wh.cpp
+++ mods/uxtheme-hook.wh.cpp
@@ -2,7 +2,7 @@
 // @id              uxtheme-hook
 // @name            UXTheme hook
 // @description     Allows you to apply custom themes
-// @version         1.2
+// @version         1.3
 // @author          rounk-ctrl
 // @github          https://github.com/rounk-ctrl
 // @include         winlogon.exe
@@ -10,7 +10,7 @@
 // @include         systemsettings.exe
 // @include         logonui.exe
 // @include         rundll32.exe
-// @compilerOptions -luxtheme -lgdi32
+// @compilerOptions -luxtheme -lgdi32 -lole32
 // ==/WindhawkMod==
 
 // ==WindhawkModReadme==
@@ -18,16 +18,24 @@
 # UXTheme hook
 apply custom themes.
 
-### ⚠ Important ⚠
+## ⚠ Important ⚠
 in order for this mod to work properly, you must include `winlogon.exe` and `logonui.exe` in windhawk's process inclusion list, in its advanced settings.
 
-### settings
+## settings
 - **control panel fix**: prevents the white images from loading in control panel. tested on windows 11, breaks on 10. disabled by default
+- **prevent settings on theme apply**: prevents settings from launching every time you apply a theme. this hooks into rundll32.exe, so if any other app uses rundll32 to launch any uwp app, it might break.
 
-### why is it not applying?
+
+## why is it not applying?
 you need to first patch the theme with an invalid signature, use themetool from secureuxtheme, select your theme and click patch, then you can apply.
 
-### bugs
+
+## theme stops applying after restart
+this is caused by logonui resetting the default colors, by reading the values located in `HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\DefaultColors\Standard`.
+this mod should fix that. if it still doesn't work, try renaming `DefaultColors` to something else.
+
+
+## bugs
 the colors in control panel might be weird sometimes. works fine if you leave control panel open, then switch themes.
 */
 // ==/WindhawkModReadme==
@@ -37,14 +45,20 @@ the colors in control panel might be weird sometimes. works fine if you leave co
 - cpanelFix: false
   $name: Fix control panel white header/sidebar
   $description: Fixes control panel hardcoding white image for header and sidebar
+
+- preventSettings: false
+  $name: Prevent settings from launching on theme apply
+  $description: Prevents settings from launching every time you apply a new theme
 */
 // ==/WindhawkModSettings==
 
 #include <vssym32.h>
+#include <windhawk_api.h>
 #include <windhawk_utils.h>
 #include <vector>
 #include <uxtheme.h>
-#include <winerror.h>
+#include <windows.h>
+#include <initguid.h>
 
 #ifdef _WIN64
 #define STDCALL  __cdecl
@@ -57,6 +71,7 @@ typedef unsigned __int64 QWORD;
 
 struct {
     bool cpanelFix;
+    bool preventSettings;
 } settings;
 
 #pragma region common
@@ -114,13 +129,41 @@ SetSysColors_t SetSysColors_orig;
 
 int WINAPI SetSysColors_hook(int cElements, const INT *lpaElements, const COLORREF *lpaRgbValues)
 {
+    // default fallback
+    DWORD elemCount = 13;
+
+    // get the number of values
+    HKEY hKey;
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE,L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\DefaultColors\\Standard", 0, KEY_READ, &hKey) == ERROR_SUCCESS)
+    {
+        RegQueryInfoKey(hKey, NULL, NULL, NULL, NULL, NULL, NULL, &elemCount, NULL, NULL, NULL, NULL);
+        RegCloseKey(hKey);
+    }
+
     // logonui
-    if (cElements == 13) return 1;
+    if (cElements == (int)elemCount) return TRUE;
     return SetSysColors_orig(cElements, lpaElements, lpaRgbValues);
 }
 
+// compatibility with windhawk 1.4
+DEFINE_GUID(IID_IApplicationActivationManager, 0x2e941141, 0x7f97, 0x4756, 0xba,0x1d, 0x9d,0xec,0xde,0x89,0x4a,0x3d);
+DEFINE_GUID(CLSID_ApplicationActivationManager, 0x45ba127d, 0x10a8, 0x46ea, 0x8a,0xb7, 0x56,0xea,0x90,0x78,0x94,0x3c);
+
+using CoCreateInstance_t = decltype(&CoCreateInstance);
+CoCreateInstance_t CoCreateInstance_orig;
+HRESULT WINAPI CoCreateInstance_hook(REFCLSID rclsid, LPUNKNOWN pUnkOuter, DWORD dwClsContext, REFIID riid, LPVOID *ppv){
+    if (IsEqualCLSID(rclsid, CLSID_ApplicationActivationManager) 
+        && IsEqualIID(riid, IID_IApplicationActivationManager))
+    {
+        return E_NOINTERFACE;
+    }
+    return CoCreateInstance_orig(rclsid, pUnkOuter, dwClsContext, riid, ppv);
+}
+
+
 void LoadSettings() {
     settings.cpanelFix = Wh_GetIntSetting(L"cpanelFix");
+    settings.preventSettings = Wh_GetIntSetting(L"preventSettings");
 }
 
 BOOL Wh_ModInit() {
@@ -168,6 +211,11 @@ BOOL Wh_ModInit() {
             Wh_Log(L"Failed to hook DUI");
             return FALSE;
         }
+    }
+
+    if (settings.preventSettings)
+    {
+        WindhawkUtils::Wh_SetFunctionHookT(CoCreateInstance, CoCreateInstance_hook, &CoCreateInstance_orig);
     }
 
     return TRUE;
