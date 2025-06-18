adccd1082036b35b4648168fdf840cc82ff1525d
aubrey <44238627+aubymori@users.noreply.github.com>
2025-06-18 14:06:11 -0500

    Custom Desktop Watermark 1.1.0 (#2034)
    - Add notice to README about empty lines
    - Add option to force a specific line to be bold
    - Add option to change the horizontal alignment of a specific line

--- mods/custom-desktop-watermark.wh.cpp
+++ mods/custom-desktop-watermark.wh.cpp
@@ -2,7 +2,7 @@
 // @id              custom-desktop-watermark
 // @name            Custom Desktop Watermark
 // @description     Lets you set your own desktop watermark text
-// @version         1.0.2
+// @version         1.1.0
 // @author          aubymori
 // @github          https://github.com/aubymori
 // @include         explorer.exe
@@ -16,13 +16,23 @@
 This mod allows you to completely customize the desktop watermark,
 putting in your own lines of text.
 
+# NOTICE
+Due to a limitation with the Windhawk settings API, an empty line will stop
+the mod from reading lines. If you want a blank line, use a space character.
+
 ## Examples
 
 **Windows 7 (not genuine)**:
+
 ![Windows 7 (not genuine)](https://raw.githubusercontent.com/aubymori/images/main/custom-desktop-watermark-win7.png)
 
 **Windows XP Professional x64 Edition**:
+
 ![Windows XP Professional x64 Edition](https://raw.githubusercontent.com/aubymori/images/main/custom-desktop-watermark-winxp.png)
+
+**Windows Technical Preview**:
+
+![Windows Technical Preview](https://raw.githubusercontent.com/aubymori/images/main/custom-desktop-watermark-wintp.png)
 */
 // ==/WindhawkModReadme==
 
@@ -34,6 +44,16 @@ putting in your own lines of text.
     - title: false
       $name: Use caption font
       $description: Use caption font instead of message font when classic fonts are enabled.
+    - bold: false
+      $name: Bold text
+      $description: Force this line to use bold text always.
+    - align: right
+      $name: Align
+      $description: The edge to align the line of text to.
+      $options:
+      - left: Left
+      - center: Center
+      - right: Right
   $name: Text lines
 - classic: false
   $name: Use classic fonts
@@ -53,10 +73,12 @@ struct WatermarkLine
 {
     std::wstring text;
     bool title;
+    bool bold;
+    int align;
 };
 
 std::vector<WatermarkLine> g_lines;
-bool g_bClassic = false;
+bool g_fClassic = false;
 
 bool (*CDesktopWatermark_s_WantWatermark_orig)(void);
 bool CDesktopWatermark_s_WantWatermark_hook(void)
@@ -64,11 +86,24 @@ bool CDesktopWatermark_s_WantWatermark_hook(void)
     return true;
 }
 
+int CalculateTextSize(HDC hDC, LPCWSTR pszText, int *lpWidth)
+{
+    RECT rcText = { 0 };
+    int height = DrawTextW(
+        hDC, pszText, -1,
+        &rcText, DT_CALCRECT | DT_SINGLELINE
+    );
+    *lpWidth = RECTWIDTH(rcText);
+    return height;
+}
+
 int PaintLine(
     HDC            hDC,
     LPCRECT        lprc,
     LPCWSTR        lpszText,
     HFONT          hFont,
+    int            align,
+    int            width,
     int            offset
 )
 {
@@ -81,7 +116,7 @@ int PaintLine(
     );
 
     RECT rcPaint = { 0 };
-    rcPaint.left = lprc->right - RECTWIDTH(rcText);
+    rcPaint.left = lprc->right - width;
     rcPaint.top = lprc->bottom - RECTHEIGHT(rcText);
     rcPaint.right = lprc->right;
     rcPaint.bottom = lprc->bottom;
@@ -89,7 +124,7 @@ int PaintLine(
     int padding = MulDiv(5, GetDeviceCaps(hDC, LOGPIXELSX), 96);
     OffsetRect(&rcPaint, -padding, -offset);
 
-    DrawTextW(hDC, lpszText, -1, &rcPaint, DT_SINGLELINE);
+    DrawTextW(hDC, lpszText, -1, &rcPaint, DT_SINGLELINE | align);
 
     SelectObject(hDC, hfOld);
     return newOffset;    
@@ -117,25 +152,53 @@ void CDesktopWatermark_s_DesktopBuildPaint_hook(
     );
     HFONT hTitleFont = CreateFontIndirectW(&ncm.lfCaptionFont);
     HFONT hMessageFont = CreateFontIndirectW(&ncm.lfMessageFont);
-    if (!hTitleFont || !hMessageFont)
+    ncm.lfCaptionFont.lfWeight = FW_BOLD;
+    ncm.lfMessageFont.lfWeight = FW_BOLD;
+    HFONT hTitleFontBold = CreateFontIndirectW(&ncm.lfCaptionFont);
+    HFONT hMessageFontBold = CreateFontIndirectW(&ncm.lfMessageFont);
+    if (!hTitleFont || !hMessageFont || !hTitleFontBold || !hMessageFontBold)
         return;
     int padding = MulDiv(3, dpi, 96);
-    offset += MulDiv(g_bClassic ? 4 : 1, dpi, 96);
+    offset += MulDiv(g_fClassic ? 4 : 1, dpi, 96);
+
+    int maxWidth = 0;
+    for (const WatermarkLine &line : g_lines)
+    {
+        bool fMessageFont = g_fClassic && !line.title;
+        HFONT hFont = fMessageFont
+        ? (line.bold ? hMessageFontBold : hMessageFont)
+        : (line.bold ? hTitleFontBold : hTitleFont);
+
+        HFONT hfOld = (HFONT)SelectObject(hDC, hFont);
+        int width;
+        CalculateTextSize(hDC, line.text.c_str(), &width);
+        SelectObject(hDC, hfOld);
+
+        if (width > maxWidth)
+            maxWidth = width;
+    }
 
     for (size_t i = g_lines.size(); i--;)
     {
-        WatermarkLine line = g_lines.at(i);
-        bool bMessageFont = g_bClassic && !line.title;
+        WatermarkLine &line = g_lines.at(i);
+        bool fMessageFont = g_fClassic && !line.title;
+        HFONT hFont = fMessageFont
+        ? (line.bold ? hMessageFontBold : hMessageFont)
+        : (line.bold ? hTitleFontBold : hTitleFont);
 
         offset += PaintLine(
             hDC, lprc, line.text.c_str(),
-            bMessageFont ? hMessageFont : hTitleFont,
+            hFont,
+            line.align,
+            maxWidth,
             offset
         ) + padding;
     }
 
     DeleteObject(hTitleFont);
     DeleteObject(hMessageFont);
+    DeleteObject(hTitleFontBold);
+    DeleteObject(hMessageFontBold);
     SetBkMode(hDC, bk);
     SetTextColor(hDC, cr);
 }
@@ -162,20 +225,32 @@ const WindhawkUtils::SYMBOL_HOOK shell32DllHooks[] = {
 void LoadSettings(void)
 {
     g_lines.clear();
-    g_bClassic = Wh_GetIntSetting(L"classic");
+    g_fClassic = Wh_GetIntSetting(L"classic");
 
     for (int i = 0;; i++)
     {
-        LPCWSTR lpszText = Wh_GetStringSetting(L"lines[%i].text", i);
-        if (!*lpszText)
+        LPCWSTR pszText = Wh_GetStringSetting(L"lines[%i].text", i);
+        if (!*pszText)
         {
-            Wh_FreeStringSetting(lpszText);
+            Wh_FreeStringSetting(pszText);
             break;
         }
         bool title = Wh_GetIntSetting(L"lines[%i].title", i);
-        WatermarkLine line = { lpszText, title };
+        bool bold = Wh_GetIntSetting(L"lines[%i].bold", i);
+        int align = DT_RIGHT;
+        LPCWSTR pszAlign = Wh_GetStringSetting(L"lines[%i].align", i);
+        if (0 == wcscmp(pszAlign, L"left"))
+        {
+            align = DT_LEFT;
+        }
+        else if (0 == wcscmp(pszAlign, L"center"))
+        {
+            align = DT_CENTER;
+        }
+        Wh_FreeStringSetting(pszAlign);
+        WatermarkLine line = { pszText, title, bold, align };
         g_lines.push_back(line);
-        Wh_FreeStringSetting(lpszText);
+        Wh_FreeStringSetting(pszText);
     }
 }
 
