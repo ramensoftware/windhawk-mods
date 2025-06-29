0574652f45cba52f250ffb813de03a0c81a2cbb8
tetawaves <130876208+tetawaves@users.noreply.github.com>
2025-06-29 23:05:47 +0200

    UIFILE Override v1.0.3 (#2077)
    * Use LoadLibraryEx with LOAD_LIBRARY_SEARCH_SYSTEM32 #2063
    * Code cleanup

--- mods/uifile-override.wh.cpp
+++ mods/uifile-override.wh.cpp
@@ -2,7 +2,7 @@
 // @id              uifile-override
 // @name            UIFILE Override
 // @description     Override UIFILE resources
-// @version         1.0.2
+// @version         1.0.3
 // @author          xalejandro
 // @github          https://github.com/tetawaves
 // @include         *
@@ -29,177 +29,188 @@ This will override the original UIFILE resource from shell32 to the one in shell
 #include <windhawk_utils.h>
 
 #ifdef _WIN64
-#define THISCALL __cdecl
-#define STHISCALL L"__cdecl"
+#   define THISCALL  __cdecl
+#   define STHISCALL L"__cdecl"
 #else
-#define THISCALL __stdcall
-#define STHISCALL L"__stdcall"
+#   define THISCALL  __thiscall
+#   define STHISCALL L"__thiscall"
 #endif
 
-WCHAR g_pszCurrentThemePath[264];
-HMODULE g_ShellStyleMod;
+WCHAR g_szCurrentThemePath[MAX_PATH + 4];
+HMODULE g_hShellStyle = nullptr;
 
-HMODULE getShellStyleHInstance() {
-    WCHAR pszThemeFileName[264];
-    WCHAR pszColorBuff[104];
-    int pszSizeBuff[108];
+HRESULT (THISCALL *_AllocArray)(void *, UINT, int, LPWSTR *);
 
-    if (!GetCurrentThemeName(pszThemeFileName, 260, pszColorBuff, 100,
-                             (LPWSTR)pszSizeBuff, MAX_PATH)) {
-        PathRemoveFileSpecW(pszThemeFileName);
-        PathAppendW(pszThemeFileName, L"Shell");
-        PathAppendW(pszThemeFileName, pszColorBuff);
-        PathAppendW(pszThemeFileName, L"shellstyle.dll");
+HMODULE getShellStyleModule()
+{
+    WCHAR szThemeFileName[MAX_PATH + 4];
+    WCHAR szColorBuff[MAX_PATH];
+    WCHAR szSizeBuff[MAX_PATH];
 
-        if (lstrcmpiW(g_pszCurrentThemePath, pszThemeFileName)) {
-            lstrcpyW(g_pszCurrentThemePath, pszThemeFileName);
-            FreeLibrary(g_ShellStyleMod);
-            g_ShellStyleMod = LoadLibraryExW(pszThemeFileName, 0, 2);
-            Wh_Log(L"shellstyle changed to %s = %p", pszThemeFileName,
-                   g_ShellStyleMod);
+    if (!GetCurrentThemeName(szThemeFileName, MAX_PATH, szColorBuff, MAX_PATH, szSizeBuff, MAX_PATH))
+    {
+        PathRemoveFileSpecW(szThemeFileName);
+        PathAppendW(szThemeFileName, L"Shell");
+        PathAppendW(szThemeFileName, szColorBuff);
+        PathAppendW(szThemeFileName, L"shellstyle.dll");
+
+        if (lstrcmpiW(g_szCurrentThemePath, szThemeFileName)) 
+        {
+            lstrcpyW(g_szCurrentThemePath, szThemeFileName);
+            g_hShellStyle = LoadLibraryExW(szThemeFileName, nullptr, LOAD_LIBRARY_AS_DATAFILE);
+            Wh_Log(L"shellstyle changed to %s = %p", szThemeFileName, g_hShellStyle);
         }
     }
-    return g_ShellStyleMod;
+
+    return g_hShellStyle;
 }
 
-HRESULT (*THISCALL AllocArray)(void*, char, int, LPWSTR*);
+HRESULT (THISCALL *DUILoadUIFileFromResources_orig)(HINSTANCE, UINT, LPWSTR *);
+HRESULT THISCALL DUILoadUIFileFromResources_hook(HINSTANCE hInstance, UINT uResId, LPWSTR* ppszOut) 
+{
+    HRSRC hResInfo = nullptr;
+    HGLOBAL hResData = nullptr;
+    LPVOID lpData = nullptr;
+    HRESULT hr = S_OK;
 
-HRESULT(*THISCALL DUILoadUIFileFromResources_orig)
-(HINSTANCE, unsigned int, LPWSTR*);
-HRESULT THISCALL DUILoadUIFileFromResources_hook(HINSTANCE hInstance,
-                                                 unsigned int uResId,
-                                                 LPWSTR* pszOut) {
-    HRSRC ResourceW;
-    HGLOBAL Resource;
-    const CHAR* resData;
-    int cwchBuf;
-    HRESULT result = 0;
-    signed int LastError;
-    HMODULE hShellStyleMod = getShellStyleHInstance();
-    LPCWSTR lpType = L"UIFILE";
+    HMODULE hShellStyle = getShellStyleModule();
 
-    if (HRSRC CustomResourceW =
-            FindResourceW(hShellStyleMod, (LPCWSTR)uResId, L"SHELL32_UIFILE");
-        CustomResourceW) {
+    if (HRSRC hResInfoOverride = FindResourceW(hShellStyle, MAKEINTRESOURCEW(uResId), L"SHELL32_UIFILE"); hResInfoOverride)
+    {
         Wh_Log(L"found %d from SHELL32_UIFILE", uResId);
-        ResourceW = CustomResourceW;
-        hInstance = hShellStyleMod;
-    } else {
-        ResourceW = FindResourceW(hInstance, (LPCWSTR)uResId, lpType);
+        hResInfo = hResInfoOverride;
+        hInstance = hShellStyle;        
+    }
+    else
+    {
+        hResInfo = FindResourceW(hInstance, MAKEINTRESOURCEW(uResId), L"UIFILE");
     }
 
-    if (ResourceW && (Resource = LoadResource(hInstance, ResourceW)) != 0 &&
-        (resData = (const CHAR*)LockResource(Resource)) != 0) {
-        cwchBuf = SizeofResource(hInstance, ResourceW) + 1;
-        result = AllocArray(NULL, 64, cwchBuf, pszOut);
-        if (result >= 0)
-            SHAnsiToUnicode(resData, *pszOut, cwchBuf);
-    } else {
-        LastError = GetLastError();
-        result = LastError;
-        if (LastError > 0)
-            return (unsigned __int16)LastError | 0x80070000;
-    }
-    return result;
-}
-
-using SetXMLFromResource_t = HRESULT (*__thiscall)(void* pThis,
-                                                   PCWSTR lpName,
-                                                   PCWSTR lpType,
-                                                   HMODULE hModule,
-                                                   HINSTANCE param4,
-                                                   HINSTANCE param5);
-SetXMLFromResource_t SetXMLFromResource_orig;
-HRESULT __thiscall SetXMLFromResource_hook(void* pThis,
-                                           PCWSTR lpName,
-                                           PCWSTR lpType,
-                                           HMODULE hModule,
-                                           HINSTANCE param4,
-                                           HINSTANCE param5) {
-    WCHAR Filename[MAX_PATH];
-    WCHAR* FilenameW;
-    HMODULE hShellStyleMod = getShellStyleHInstance();
-    HRESULT result = 0;
-    if (!lstrcmpW(lpType, L"UIFILE")) {
-        GetModuleFileNameW(hModule, Filename, MAX_PATH);
-        FilenameW = PathFindFileNameW(Filename);
-        if (FilenameW) {
-            PathRemoveExtensionW(FilenameW);
-            CharUpperW(FilenameW);
-            lstrcatW(FilenameW, L"_");
-            StrNCatW(FilenameW, lpType, MAX_PATH);
-            result = SetXMLFromResource_orig(pThis, lpName, FilenameW,
-                                             hShellStyleMod, param4, param5);
-
-            if (SUCCEEDED(result)) {
-                Wh_Log(L"found %d from %s", lpName, FilenameW);
-                // Fix for strings
-                *((HINSTANCE*)pThis + 3) = hModule;
-                return result;
-            }
+    if (hResInfo && (hResData = LoadResource(hInstance, hResInfo)) && (lpData = LockResource(hResData))) 
+    {
+        int cwchBuf = SizeofResource(hInstance, hResInfo) + 1;
+        hr = _AllocArray(nullptr, 64, cwchBuf, ppszOut);
+        if (SUCCEEDED(hr))
+        {
+            SHAnsiToUnicode((const CHAR *)lpData, *ppszOut, cwchBuf);
+        }
+    } 
+    else 
+    {
+        DWORD LastError = GetLastError();
+        if (LastError)
+        {
+            return HRESULT_FROM_WIN32(LastError);
         }
     }
-    return SetXMLFromResource_orig(pThis, lpName, lpType, hModule, param4,
-                                   param5);
+
+    return hr;
 }
 
-BOOL Wh_ModInit() {
+HRESULT (__thiscall *SetXMLFromResource_orig)(void* pThis, PCWSTR lpName, PCWSTR lpType, HMODULE hModule, HINSTANCE param4, HINSTANCE param5);
+HRESULT __thiscall SetXMLFromResource_hook(void* pThis, PCWSTR lpName, PCWSTR lpType, HMODULE hModule, HINSTANCE param4, HINSTANCE param5) 
+{
+    WCHAR szModuleName[MAX_PATH];
+    LPWSTR pszFilePath = nullptr;
+    HMODULE hShellStyle = getShellStyleModule();
+
+    if (!lstrcmpW(lpType, L"UIFILE"))
+    {
+        GetModuleFileNameW(hModule, szModuleName, MAX_PATH);
+        pszFilePath = PathFindFileNameW(szModuleName);
+        if (pszFilePath)
+        {
+            PathRemoveExtensionW(pszFilePath);
+            CharUpperW(pszFilePath);
+            lstrcatW(pszFilePath, L"_");
+            StrNCatW(pszFilePath, lpType, MAX_PATH);
+            if (HRESULT hr = SetXMLFromResource_orig(pThis, lpName, pszFilePath, hShellStyle, param4, param5); SUCCEEDED(hr))
+            {
+                Wh_Log(L"found %d from %s", lpName, pszFilePath);
+                // Fix for strings
+                *((HMODULE*)pThis + 3) = hModule;
+                return hr;
+            }          
+        }
+    }
+
+    return SetXMLFromResource_orig(pThis, lpName, lpType, hModule, param4, param5);
+}
+
+BOOL Wh_ModInit() 
+{
     Wh_Log(L"Init");
 
-    HMODULE hMod = GetModuleHandleW(NULL);
-    wchar_t exePath[MAX_PATH];
-    GetModuleFileName(hMod, exePath, MAX_PATH);
+    HMODULE hModule = GetModuleHandleW(nullptr);
+    wchar_t szModulePath[MAX_PATH];
+    GetModuleFileNameW(hModule, szModulePath, MAX_PATH);
 
-    HMODULE duiModule = LoadLibrary(L"dui70.dll");
-    if (duiModule) {
+    HMODULE duiModule = LoadLibraryExW(L"dui70.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+    if (duiModule) 
+    {
         PCSTR procName =
             R"(?_SetXMLFromResource@DUIXmlParser@DirectUI@@IAEJPBG0PAUHINSTANCE__@@11@Z)";
         FARPROC pSetXMLFromResource = GetProcAddress(duiModule, procName);
-        if (!pSetXMLFromResource) {
-#ifdef _WIN64
+        if (!pSetXMLFromResource) 
+        {
+            #ifdef _WIN64
             PCSTR procName_Win10_x64 =
                 R"(?_SetXMLFromResource@DUIXmlParser@DirectUI@@IEAAJPEBG0PEAUHINSTANCE__@@11@Z)";
             pSetXMLFromResource = GetProcAddress(duiModule, procName_Win10_x64);
-#endif
+            #endif
         }
 
-        if (pSetXMLFromResource) {
+        if (pSetXMLFromResource) 
+        {
             Wh_SetFunctionHook((void*)pSetXMLFromResource,
                                (void*)SetXMLFromResource_hook,
                                (void**)&SetXMLFromResource_orig);
-        } else {
+        } 
+        else 
+        {
             Wh_Log(L"Couldn't find SetXMLFromResource");
         }
-    } else {
+    } 
+    else 
+    {
         Wh_Log(L"Failed to load dui70.dll");
     }
 
-    if (lstrcmpiW(L"explorer.exe", PathFindFileNameW(exePath))) {
+    if (lstrcmpiW(L"explorer.exe", PathFindFileNameW(szModulePath))) 
+    {
         return TRUE;
     }
 
-    HMODULE hShell32 = LoadLibraryW(L"shell32.dll");
-    if (!hShell32) {
+    HMODULE hShell32 = LoadLibraryExW(L"shell32.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+    if (!hShell32) 
+    {
         Wh_Log(L"Failed to load shell32.dll");
         return FALSE;
     }
 
-    WindhawkUtils::SYMBOL_HOOK shell32DllHooks[] = {
-        {{L"long " STHISCALL " DUI_LoadUIFileFromResources(struct HINSTANCE__ "
-          "*,unsigned int,unsigned short * *)"},
-         (void**)&DUILoadUIFileFromResources_orig,
-         (void*)DUILoadUIFileFromResources_hook,
-         FALSE},
-
-        {{L"long " STHISCALL
-          " _AllocArray<unsigned short,class CTGlobalAllocPolicy>(void "
-          "*,unsigned long,unsigned __int64,unsigned short * *)"},
-         (void**)&AllocArray,
-         FALSE}
-
+    WindhawkUtils::SYMBOL_HOOK shell32DllHooks[]
+    {
+        {
+            {
+                L"long " STHISCALL " DUI_LoadUIFileFromResources(struct HINSTANCE__ *,unsigned int,unsigned short * *)"
+            },
+            &DUILoadUIFileFromResources_orig,
+            DUILoadUIFileFromResources_hook,
+            false
+        },
+        {
+            {
+                L"long " STHISCALL " _AllocArray<unsigned short,class CTLocalAllocPolicy>(void *,unsigned long,unsigned __int64,unsigned short * *)"
+            },
+            &_AllocArray,
+            nullptr,
+            false
+        }
     };
 
-    if (!WindhawkUtils::HookSymbols(hShell32, shell32DllHooks, 2)) {
+    if (!WindhawkUtils::HookSymbols(hShell32, shell32DllHooks,
+                                    ARRAYSIZE(shell32DllHooks))) 
+    {
         Wh_Log(L"Failed to hook shell32.dll");
         return FALSE;
     }
@@ -207,8 +218,10 @@ BOOL Wh_ModInit() {
     return TRUE;
 }
 
-void Wh_ModUninit(void) {
-    if (g_ShellStyleMod) {
-        FreeLibrary(g_ShellStyleMod);
+void Wh_ModUninit(void) 
+{
+    if (g_hShellStyle) 
+    {
+        FreeLibrary(g_hShellStyle);
     }
 }
