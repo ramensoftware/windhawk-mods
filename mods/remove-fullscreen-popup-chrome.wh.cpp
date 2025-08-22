f851b1fe70b014e3ed2ec05c209d89ef690f76d8
lorenzoc01 <lorecappe2001@gmail.com>
2025-08-22 21:43:06 +0200

    Improve window-finding logic (#2334)

--- mods/remove-fullscreen-popup-chrome.wh.cpp
+++ mods/remove-fullscreen-popup-chrome.wh.cpp
@@ -1,8 +1,8 @@
 // ==WindhawkMod==
 // @id              remove-fullscreen-popup-chrome
-// @name            Remove "To Exit Fullscreen Press Esc" popup - Chrome/Opera
-// @description     Remove “TO EXIT FULLSCREEN PRESS ESC/F11” popup in Chrome/Opera/OperaGX
-// @version         1.0
+// @name            Remove "Exit Fullscreen" popup in Chrome
+// @description     Removes “TO EXIT FULLSCREEN PRESS ESC/F11” popup in Chrome/Opera/OperaGX
+// @version         1.1
 // @author          lorenzoc01
 // @github          https://github.com/lorenzoc01
 // @include         opera.exe
@@ -12,7 +12,7 @@
 
 // ==WindhawkModReadme==
 /*
-# Remove "To Exit Fullscreen Press Esc" popup – Chrome/Opera/OperaGX
+# Remove "Exit Fullscreen" popup in Chrome/Opera/OperaGX
 
 A simple mod to remove the annoying “TO EXIT FULLSCREEN PRESS ESC/F11” popup in Chrome, Opera and OperaGX.
 
@@ -30,76 +30,41 @@ Check the [Github Repo](https://github.com/lorenzoc01/remove-chrome-fullscreen-p
 
 #include <commctrl.h>
 #include <windhawk_api.h>
+#include <windows.h>
 #include <cstddef>
 
-HWND popupWindowhandle = nullptr;
-
-using CreateWindowExW_t = decltype(&CreateWindowExW);
-CreateWindowExW_t pOriginalCreateWindowExW;
-HWND WINAPI CreateWindowExWHook(
-    DWORD dwExStyle,
-    LPCWSTR lpClassName,
-    LPCWSTR lpWindowName,
-    DWORD dwStyle,
-    int X,
-    int Y,
-    int nWidth,
-    int nHeight,
-    HWND hWndParent,
-    HMENU hMenu,
-    HINSTANCE hInstance,
-    LPVOID lpParam)
-{
-
-    HWND hWnd = pOriginalCreateWindowExW(
-        dwExStyle,
-        lpClassName,
-        lpWindowName,
-        dwStyle,
-        X,
-        Y,
-        nWidth,
-        nHeight,
-        hWndParent,
-        hMenu,
-        hInstance,
-        lpParam
-    );
-
-    //According to my testings, the popup has
-    // hWndParent == nullptr
-    // lpWindowName == nullptr
-    // className == "Chrome_WidgetWin_1"
-    // height == 0
-
-    if (hWndParent == nullptr && lpWindowName == nullptr){ 
-        wchar_t className[256] = {0};
-        if (GetClassNameW(hWnd, className, _countof(className)) > 0) {
-            if (wcscmp(className, L"Chrome_WidgetWin_1") == 0) {
-                RECT rect;
-                if (GetWindowRect(hWnd, &rect)) {
-                    if (rect.bottom-rect.top == 0) {
-                        Wh_Log(L"Found handle of popup window: %llX", (unsigned long long)hWnd);
-                        popupWindowhandle = hWnd;
-                    }
-                }
-            }
-        }
-    }
-
-    return hWnd;
-}
 
 using ShowWindow_t = decltype(&ShowWindow);
 ShowWindow_t pOriginalShowWindow;
 BOOL WINAPI ShowWindowHook(
     HWND hWnd,
     int nCmdShow)
-{                    
-    if (hWnd == popupWindowhandle) {
-        Wh_Log(L"Not showing window %llX", (unsigned long long)hWnd);
-        return TRUE;
+{
+
+    //According to my testings, the popup has
+    // className == "Chrome_WidgetWin_1"
+    // GWL_STYLE: WS_POPUP  (GWL_STYLE value: 0x86000000)
+    // GWL_EXSTYLE: WS_EX_TOOLWINDOW, WS_EX_NOACTIVATE, WS_EX_TOPMOST, WS_EX_TRANSPARENT  (GWL_EXSTYLE value: 0x80000a8)
+    
+    // It is quite unlikely but if another window has the same properties, it will be hidden as well
+    
+    LONG exStyle = GetWindowLong(hWnd, GWL_EXSTYLE);
+    if ((exStyle & (WS_EX_TOOLWINDOW | WS_EX_NOACTIVATE | WS_EX_TOPMOST | WS_EX_TRANSPARENT)) == (WS_EX_TOOLWINDOW | WS_EX_NOACTIVATE | WS_EX_TOPMOST | WS_EX_TRANSPARENT)) {
+
+        LONG style = GetWindowLong(hWnd, GWL_STYLE);
+        if (style & WS_POPUP && !(style & WS_SYSMENU)) {
+
+            wchar_t className[256] = {0};
+            if (GetClassNameW(hWnd, className, _countof(className)) > 0) {
+                if (wcscmp(className, L"Chrome_WidgetWin_1") == 0) {
+
+                    Wh_Log(L"Not showing window: %llX", (unsigned long long)hWnd);
+                    return TRUE;
+                }
+            }
+        }
     }
+
     return pOriginalShowWindow(hWnd, nCmdShow);
 }
 
@@ -107,7 +72,6 @@ BOOL Wh_ModInit(void)
 {
     Wh_Log(L"Mod Init");
 
-    Wh_SetFunctionHook((void*)CreateWindowExW, (void*)CreateWindowExWHook, (void**)&pOriginalCreateWindowExW);
     Wh_SetFunctionHook((void*)ShowWindow, (void*)ShowWindowHook, (void**)&pOriginalShowWindow);
 
     return TRUE;
@@ -116,6 +80,4 @@ BOOL Wh_ModInit(void)
 void Wh_ModUninit(void)
 {
     Wh_Log(L"Mod Uninit");
-    Wh_RemoveFunctionHook((void *)CreateWindowExW);
-    Wh_RemoveFunctionHook((void*)ShowWindow);
 }
