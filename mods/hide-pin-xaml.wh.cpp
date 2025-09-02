c4278c84b89f8804045eb76df2e1f845ccb107bf
Dan Alec <danalec@gmail.com>
2025-09-02 00:01:27 -0300

    add: hide-pin-xaml

---
+++ mods/hide-pin-xaml.wh.cpp
@@ -0,0 +1,294 @@
+// ==WindhawkMod==
+// @id             hide-pin-xaml
+// @name           Hide Taskbar Pin Icons (on Explorer Quick Access)
+// @description    Hide the pin/unpin icons in Windows 11 taskbar using XAML visual tree monitoring
+// @version        1.0.0
+// @author         @danalec
+// @github         https://github.com/danalec/windhawk-mods
+// @include        explorer.exe
+// @compilerOptions -lole32 -loleaut32 -lruntimeobject
+// ==/WindhawkMod==
+// ==WindhawkModSettings==
+// ==/WindhawkModSettings==
+
+#include <windows.h>
+#include <unknwn.h>
+#include <oleauto.h>
+#include <XamlOM.h>
+#include <ocidl.h>
+
+#ifdef GetCurrentTime
+#undef GetCurrentTime
+#endif
+
+#include <winrt/base.h>
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Media.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Media;
+using namespace Microsoft::UI::Xaml::Controls;
+namespace wf = winrt::Windows::Foundation;
+
+static const CLSID CLSID_WindhawkTAP = { 0x41c21ea0, 0x7a10, 0x4b2f, { 0xa6, 0xb5, 0xd4, 0xe7, 0x9e, 0x9a, 0xcd, 0xc4 }};
+
+namespace {
+
+class VisualTreeWatcher : public winrt::implements<VisualTreeWatcher, IVisualTreeServiceCallback2, winrt::non_agile>
+{
+public:
+    VisualTreeWatcher(winrt::com_ptr<IUnknown> site);
+    VisualTreeWatcher(const VisualTreeWatcher&) = delete;
+    VisualTreeWatcher& operator=(const VisualTreeWatcher&) = delete;
+    VisualTreeWatcher(VisualTreeWatcher&&) = delete;
+    VisualTreeWatcher& operator=(VisualTreeWatcher&&) = delete;
+    ~VisualTreeWatcher() = default;
+    void UnadviseVisualTreeChange() {}
+
+private:
+    HRESULT STDMETHODCALLTYPE OnVisualTreeChange(ParentChildRelation relation, VisualElement element, VisualMutationType mutationType) override;
+    HRESULT STDMETHODCALLTYPE OnElementStateChanged(InstanceHandle element, VisualElementState elementState, LPCWSTR context) noexcept override;
+
+    wf::IInspectable FromHandle(InstanceHandle handle)
+    {
+        wf::IInspectable obj;
+        winrt::check_hresult(m_XamlDiagnostics->GetIInspectableFromHandle(handle, reinterpret_cast<::IInspectable**>(winrt::put_abi(obj))));
+        return obj;
+    }
+
+    winrt::com_ptr<IXamlDiagnostics> m_XamlDiagnostics = nullptr;
+};
+
+VisualTreeWatcher::VisualTreeWatcher(winrt::com_ptr<IUnknown> site) :
+    m_XamlDiagnostics(site.as<IXamlDiagnostics>())
+{
+    if (m_XamlDiagnostics)
+    {
+        auto visualTreeService = m_XamlDiagnostics.as<IVisualTreeService>();
+        if (visualTreeService)
+        {
+            visualTreeService->AdviseVisualTreeChange(this);
+        }
+    }
+}
+
+HRESULT VisualTreeWatcher::OnVisualTreeChange(ParentChildRelation, VisualElement element, VisualMutationType mutationType)
+{
+    try {
+        if (mutationType != Add) {
+            return S_OK;
+        }
+
+        auto elementObj = FromHandle(element.Handle);
+        if (!elementObj) {
+            return S_OK;
+        }
+
+        auto frameworkElement = elementObj.try_as<FrameworkElement>();
+        if (!frameworkElement) {
+            return S_OK;
+        }
+
+        if (auto fontIcon = frameworkElement.try_as<FontIcon>()) {
+            auto elementName = fontIcon.Name();
+            if (elementName == L"PinnedGlyphFontIcon") {
+                auto parent = VisualTreeHelper::GetParent(frameworkElement.as<DependencyObject>());
+                if (parent) {
+                    if (auto parentElement = parent.try_as<FrameworkElement>()) {
+                        parentElement.Visibility(Visibility::Collapsed);
+                    }
+                }
+            }
+        }
+        
+        return S_OK;
+    }
+    catch (...) {
+        return E_FAIL;
+    }
+}
+
+HRESULT VisualTreeWatcher::OnElementStateChanged(InstanceHandle element, VisualElementState elementState, LPCWSTR context) noexcept
+{
+    return S_OK;
+}
+
+winrt::com_ptr<VisualTreeWatcher> g_visualTreeWatcher;
+
+HMODULE GetCurrentModuleHandle() {
+    HMODULE module;
+    if (!GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
+                               GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+                           L"", &module)) {
+        return nullptr;
+    }
+    return module;
+}
+
+class WindhawkTAP : public winrt::implements<WindhawkTAP, IObjectWithSite, winrt::non_agile>
+{
+public:
+    HRESULT STDMETHODCALLTYPE SetSite(IUnknown *pUnkSite) override;
+    HRESULT STDMETHODCALLTYPE GetSite(REFIID riid, void **ppvSite) noexcept override;
+
+private:
+    winrt::com_ptr<IUnknown> site;
+};
+
+HRESULT WindhawkTAP::SetSite(IUnknown *pUnkSite) try
+{
+    if (g_visualTreeWatcher)
+    {
+        g_visualTreeWatcher->UnadviseVisualTreeChange();
+        g_visualTreeWatcher = nullptr;
+    }
+
+    site.copy_from(pUnkSite);
+
+    if (site)
+    {
+        FreeLibrary(GetCurrentModuleHandle());
+        g_visualTreeWatcher = winrt::make_self<VisualTreeWatcher>(site);
+    }
+
+    return S_OK;
+}
+catch (...)
+{
+    HRESULT hr = winrt::to_hresult();
+    Wh_Log(L"Error %08X", hr);
+    return hr;
+}
+
+HRESULT WindhawkTAP::GetSite(REFIID riid, void **ppvSite) noexcept
+{
+    return site.as(riid, ppvSite);
+}
+
+template<class T>
+struct SimpleFactory : winrt::implements<SimpleFactory<T>, IClassFactory, winrt::non_agile>
+{
+    HRESULT STDMETHODCALLTYPE CreateInstance(IUnknown* pUnkOuter, REFIID riid, void** ppvObject) override try
+    {
+        if (!pUnkOuter)
+        {
+            *ppvObject = nullptr;
+            return winrt::make<T>().as(riid, ppvObject);
+        }
+        else
+        {
+            return CLASS_E_NOAGGREGATION;
+        }
+    }
+    catch (...)
+    {
+        HRESULT hr = winrt::to_hresult();
+        Wh_Log(L"Error %08X", hr);
+        return hr;
+    }
+
+    HRESULT STDMETHODCALLTYPE LockServer(BOOL fLock) noexcept override
+    {
+        return S_OK;
+    }
+};
+
+using PFN_INITIALIZE_XAML_DIAGNOSTICS_EX = decltype(&InitializeXamlDiagnosticsEx);
+
+HRESULT InjectWindhawkTAP() noexcept
+{
+    HMODULE module = GetCurrentModuleHandle();
+    if (!module)
+    {
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+
+    WCHAR location[MAX_PATH];
+    switch (GetModuleFileName(module, location, ARRAYSIZE(location)))
+    {
+    case 0:
+    case ARRAYSIZE(location):
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+
+    const HMODULE wux(GetModuleHandle(L"Microsoft.Internal.FrameworkUdk.dll"));
+    if (!wux) [[unlikely]]
+    {
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+
+    const auto ixde = reinterpret_cast<PFN_INITIALIZE_XAML_DIAGNOSTICS_EX>(GetProcAddress(wux, "InitializeXamlDiagnosticsEx"));
+    if (!ixde) [[unlikely]]
+    {
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+
+    HRESULT hr;
+    for (int i = 0; i < 10000; i++)
+    {
+        WCHAR connectionName[256];
+        wsprintf(connectionName, L"WinUIVisualDiagConnection%d", i + 1);
+
+        hr = ixde(connectionName, GetCurrentProcessId(), L"", location, CLSID_WindhawkTAP, nullptr);
+        if (hr != HRESULT_FROM_WIN32(ERROR_NOT_FOUND))
+        {
+            break;
+        }
+    }
+
+    return hr;
+}
+
+#include <combaseapi.h>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdll-attribute-on-redeclaration"
+
+__declspec(dllexport)
+_Use_decl_annotations_ STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv) try
+{
+    if (rclsid == CLSID_WindhawkTAP)
+    {
+        *ppv = nullptr;
+        return winrt::make<SimpleFactory<WindhawkTAP>>().as(riid, ppv);
+    }
+    else
+    {
+        return CLASS_E_CLASSNOTAVAILABLE;
+    }
+}
+catch (...)
+{
+    HRESULT hr = winrt::to_hresult();
+    Wh_Log(L"Error %08X", hr);
+    return hr;
+}
+
+__declspec(dllexport)
+_Use_decl_annotations_ STDAPI DllCanUnloadNow(void)
+{
+    if (winrt::get_module_lock())
+    {
+        return S_FALSE;
+    }
+    else
+    {
+        return S_OK;
+    }
+}
+
+#pragma clang diagnostic pop
+
+} 
+
+BOOL Wh_ModInit() {
+    try {
+        HRESULT hr = InjectWindhawkTAP();
+        return SUCCEEDED(hr) ? TRUE : FALSE;
+    }
+    catch (...) {
+        return FALSE;
+    }
+}
