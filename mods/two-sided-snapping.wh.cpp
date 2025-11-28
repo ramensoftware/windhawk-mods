7bbbf2a7e942d5070414f9744863f7049c342779
Anixx <neptunia@mail.ru>
2025-11-28 16:29:25 +0200

    Update two-sided-snapping.wh.cpp

--- mods/two-sided-snapping.wh.cpp
+++ mods/two-sided-snapping.wh.cpp
@@ -1,6 +1,6 @@
 // ==WindhawkMod==
 // @id              two-sided-snapping
-// @name            Two-sided snapping
+// @name            Two-sided snapping in Win11
 // @description     Disables Windows corner snap (quadrants) while keeping snap to the sides.
 // @version         1.0.0
 // @author          Anixx
@@ -14,7 +14,8 @@
 # Disable Aero Snap Quadrants
 
 This mod disables the corner snapping and enables the two-sided snapping, thus
-restoring the behavior before Windows 10. Restart File Explorer after enabling.
+restoring the behavior before Windows 10. This mod works only on Windows 11. 
+Restart File Explorer after enabling.
 
 This mod is ported from Explorer Patcher.
 
