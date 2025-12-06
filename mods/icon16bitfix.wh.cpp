fdf94151054dc7ee03fd794eb79ea1caa467c436
Anixx <neptunia@mail.ru>
2025-12-06 17:44:30 +0200

    Bump version to 1.0.3

--- mods/icon16bitfix.wh.cpp
+++ mods/icon16bitfix.wh.cpp
@@ -2,7 +2,7 @@
 // @id              icon16bitfix
 // @name            Icons of Win16 apps in Explorer
 // @description     Adds support for icons of 16-bit (Win16) applications in File Explorer
-// @version         1.0.2
+// @version         1.0.3
 // @author          anixx
 // @github          https://github.com/Anixx
 // @include         explorer.exe
