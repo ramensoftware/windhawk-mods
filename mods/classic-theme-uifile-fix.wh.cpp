e60778e52d2aeea1d6c72903fb7f79aa7020a35d
Anixx <neptunia@mail.ru>
2025-12-11 10:06:38 +0200

    Update classic-theme-uifile-fix.wh.cpp (#2792)
    - Avoid replacing gtc's which have CONTROLPANELSTYLE inside so to address issue of hyperlinks becoming black
    
    - If Explorer is themed, just do nothing

--- mods/classic-theme-uifile-fix.wh.cpp
+++ mods/classic-theme-uifile-fix.wh.cpp
@@ -2,10 +2,11 @@
 // @id              classic-theme-uifile-fix
 // @name            Classic Theme UIFILE Fix
 // @description     Fixes Control Panel pages that don't display in Windows Classic theme
-// @version         1.0.0
+// @version         1.1.0
 // @author          Anixx
 // @github          https://github.com/Anixx
 // @include         explorer.exe
+// @compilerOptions -luxtheme
 // ==/WindhawkMod==
 
 // ==WindhawkModReadme==
@@ -26,6 +27,7 @@ when using the Windows Classic theme.
 #include <windhawk_utils.h>
 #include <windows.h>
 #include <string>
+#include <uxtheme.h>
 
 size_t FindMatchingParen(const std::wstring& str, size_t openPos) {
     int depth = 1;
@@ -39,25 +41,31 @@ size_t FindMatchingParen(const std::wstring& str, size_t openPos) {
     return std::wstring::npos;
 }
 
-std::wstring ReplaceFunction(const std::wstring& input, const wchar_t* funcName, const wchar_t* replacement) {
+std::wstring FixGtcExceptControlPanelStyle(const std::wstring& input) {
     std::wstring result = input;
-    std::wstring searchStr = std::wstring(funcName) + L"(";
     size_t pos = 0;
     
-    while ((pos = result.find(searchStr, pos)) != std::wstring::npos) {
+    while ((pos = result.find(L"gtc(", pos)) != std::wstring::npos) {
         if (pos > 0 && (iswalnum(result[pos - 1]) || result[pos - 1] == L'_')) {
             pos++;
             continue;
         }
         
-        size_t closeParen = FindMatchingParen(result, pos + wcslen(funcName));
+        size_t closeParen = FindMatchingParen(result, pos + 3);
         if (closeParen == std::wstring::npos) {
             pos++;
             continue;
         }
         
-        result = result.substr(0, pos) + replacement + result.substr(closeParen + 1);
-        pos += wcslen(replacement);
+        std::wstring gtcCall = result.substr(pos, closeParen - pos + 1);
+        
+        if (gtcCall.find(L"CONTROLPANELSTYLE") != std::wstring::npos) {
+            pos = closeParen + 1;
+            continue;
+        }
+        
+        result = result.substr(0, pos) + L"window" + result.substr(closeParen + 1);
+        pos += 6;
     }
     
     return result;
@@ -73,7 +81,6 @@ std::wstring WrapBareDtb(const std::wstring& input) {
             continue;
         }
         
-        // Check if inside themeable()
         size_t themeablePos = result.rfind(L"themeable(", pos);
         if (themeablePos != std::wstring::npos && themeablePos >= (pos > 50 ? pos - 50 : 0)) {
             size_t themeableClose = FindMatchingParen(result, themeablePos + 9);
@@ -103,12 +110,12 @@ using SetXML_t = HRESULT(WINAPI*)(void*, const WCHAR*, HINSTANCE, HINSTANCE);
 SetXML_t g_origSetXML = nullptr;
 
 HRESULT WINAPI Hook_SetXML(void* pThis, const WCHAR* pszXML, HINSTANCE hInstance, HINSTANCE hInstance2) {
-    if (!pszXML || (!wcsstr(pszXML, L"gtc(") && !wcsstr(pszXML, L"dtb("))) {
+    if (!pszXML || IsAppThemed() || (!wcsstr(pszXML, L"gtc(") && !wcsstr(pszXML, L"dtb("))) {
         return g_origSetXML(pThis, pszXML, hInstance, hInstance2);
     }
     
     std::wstring fixed(pszXML);
-    fixed = ReplaceFunction(fixed, L"gtc", L"windowtext");
+    fixed = FixGtcExceptControlPanelStyle(fixed);
     fixed = WrapBareDtb(fixed);
     
     return g_origSetXML(pThis, fixed.c_str(), hInstance, hInstance2);
