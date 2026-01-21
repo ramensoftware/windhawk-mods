60f5564c1ecf3429bc047af37fa495af914e2431
Nikita Leshenko <nikita@leshenko.net>
2026-01-21 22:29:09 +0200

    WinDbg No OleFlushClipboard: Add details to README

--- mods/windbg-no-ole-flush-clipboard.wh.cpp
+++ mods/windbg-no-ole-flush-clipboard.wh.cpp
@@ -2,7 +2,7 @@
 // @id              windbg-no-ole-flush-clipboard
 // @name            WinDbg No OleFlushClipboard
 // @description     Prevents the UI from getting stuck on Ctrl-C
-// @version         0.1
+// @version         0.2
 // @author          Nikita
 // @github          https://github.com/nikital
 // @homepage        https://www.leshenko.net/
@@ -14,10 +14,25 @@
 # WinDbg No OleFlushClipboard
 Prevents the UI from getting stuck on Ctrl-C…
 This happens when a clipboard monitor tries to read the clipboard as soon as WinDbg copies something.
-One example is when running SPICE’s vdagent (Windows VM on Linux).
+
+In the mod author's case, it happens when running a Windows VM on Linux. The
+participants are WinDbg, WPF, and SPICE's vdagent, where:
+
+- WinDbg calls WPF's Clipboard.SetDataObject() with copy = true. This call is
+  done on WinDbg's UI thread.
+- WPF puts delay-rendered content on the clipboard and repeatedly calls
+  OleFlushClipboard(), on the aforementioned UI thread.
+- Meanwhile, SPICE's vdagent tries to synchronize the clipboard to Linux, which
+  triggers delayed rendering, but it can't complete since WinDbg UI's thread is
+  blocked.
+- WinDbg can't complete the clipboard flush since vdagent holds the clipboard
+  open. This creates a deadlock.
 
 This mod patches OleFlushClipboard to do nothing in WinDbg's UI process.
-(Will update with a full writeup once it's published.)
+
+A full technial writeup is available at:
+https://www.island.io/blog/debugging-windbg-with-windbg-fixing-a-ctrl-c-ui-freeze
+
 */
 // ==/WindhawkModReadme==
 
