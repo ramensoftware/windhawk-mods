b5e96725bd75d6a16d08cc9b3911f9dd490ef3c6
Anixx <neptunia@mail.ru>
2026-01-27 14:32:39 +0200

    Update mod name, description, and version (#3147)

--- mods/win10-taskbar-on-win11-24h2.wh.cpp
+++ mods/win10-taskbar-on-win11-24h2.wh.cpp
@@ -1,8 +1,8 @@
 // ==WindhawkMod==
 // @id              win10-taskbar-on-win11-24h2
-// @name            Win10 taskbar on Win11 24H2
-// @description     Installs Windows 10 taskbar on Windows 11 version 24H2, Windows Server 2025 and Windows 11 IoT Enterprise LTSC 2024
-// @version         0.1.3
+// @name            Win10 taskbar on Win11 24H2 or 25H2
+// @description     Installs Windows 10 taskbar on Windows 11 version 24H2, 25H2, Windows Server 2025 and Windows 11 IoT Enterprise LTSC 2024
+// @version         0.1.4
 // @architecture    x86-64
 // @author          Anixx
 // @github          https://github.com/Anixx
@@ -12,9 +12,9 @@
 
 // ==WindhawkModReadme==
 /*
-Installs Windows 10 taskbar on Windows 11 version 24H2, Windows Server 2025 and Windows 11 IoT Enterprise LTSC 2024.
+Installs Windows 10 taskbar on Windows 11 version 24H2, 25H2, Windows Server 2025 and Windows 11 IoT Enterprise LTSC 2024.
 If you are on Windows 11 version 21H2 to 23H2, you should not use this mod, but rather install the mod "Windows 10 taskbar on
-Windows 11".
+Windows 11". This mod may work on later Windows versions as well.
 
 **Important! Before enabling this mod, install the mod "Fake Explorer path".**
 
@@ -40,6 +40,8 @@ Known issues:
 * Notification toasts may cause Explorer to crash, it is recommended to disable notifications.
 
 * The keyboard layout indicator displays the language name vertically rather than horizontally.
+
+* Some users reported that the right-click menu of the taskbar does not appear, but this is inconsistens and other factors may affect this issue.
 
 */
 // ==/WindhawkModReadme==
