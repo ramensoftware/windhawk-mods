a8409bf5c43a28251914a179d8382705367fe8ee
灵弦 <3257884944@qq.com>
2026-02-27 00:04:20 +0800

    Add screenshot

--- mods/win11-power-buttons.wh.cpp
+++ mods/win11-power-buttons.wh.cpp
@@ -20,6 +20,8 @@
 
 Adds customizable one-click Shutdown, Restart, Sign out, Sleep, and Hibernate buttons to the Windows 11 Start menu, replacing the default power flyout.
 
+![](https://i.imgur.com/M7xcCgb.png)
+
 ---
 
 添加可配置的一键关机/重启/注销/睡眠/休眠按钮到 Windows 11 开始菜单，替换默认的电源按钮二级菜单。
