f9ea69b8b2503be2d072e8bc92dd7a98bc52e8be
Anixx <neptunia@mail.ru>
2026-02-26 13:16:44 +0200

    Add mod to fix taskbar keyboard layout indicator (#3403)
    * Add mod to fix taskbar keyboard layout indicator
    
    This mod fixes the keyboard layout indicator text orientation when using the classic Windows taskbar from Windows 10 running under Windows 11 24H2.
    
    * Use system font instead of Tahoma
    
    Use system font instead of Tahoma
    
    * Add architecture field to mod header
    
    Added architecture information to the mod header.

---
+++ mods/fix-legacy-taskbar-tray-input-indicator.wh.cpp
@@ -0,0 +1,121 @@
+// ==WindhawkMod==
+// @id              fix-legacy-taskbar-tray-input-indicator
+// @name            Fix language indicator in Win10 taskbar under Win11 24H2+
+// @description     Fixes text orientation in the keyboard layout indicator in Win10 taskbar running under Win11 24H2+
+// @version         1.0.0
+// @author          Anixx
+// @github          https://github.com/Anixx
+// @architecture    x86-64
+// @include         explorer.exe
+// @compilerOptions -lgdi32 -luser32
+// ==/WindhawkMod==
+
+// ==WindhawkModReadme==
+/*
+# Fix language indicator in Win10 taskbar under Win11 24H2 or later
+
+Before:
+
+![Before](https://i.imgur.com/k8e2zsh.png)
+
+After:
+
+![After](https://i.imgur.com/bsfyiYr.png)
+
+This mod fixes the keyboard layout indicator text orientation 
+when using the classic (non-modern) Windows taskbar.
+
+**Note:** This mod only works with the classic taskbar, not the modern 
+Windows 11 taskbar.
+*/
+// ==/WindhawkModReadme==
+
+#include <windows.h>
+
+static wchar_t g_lastText[8] = {};
+static DWORD g_captureTime = 0;
+static bool g_drawn = false;
+static LOGFONTW g_lastFont = {};
+
+static bool LooksLikeLayoutText(LPCWSTR lpchText, int len) {
+    if (!lpchText || len < 2 || len > 4) return false;
+    for (int i = 0; i < len; i++) {
+        if (!IsCharAlphaW(lpchText[i])) return false;
+    }
+    return true;
+}
+
+static bool IsRotatedFont(HDC hdc) {
+    HFONT hFont = (HFONT)GetCurrentObject(hdc, OBJ_FONT);
+    if (!hFont) return false;
+    if (GetObjectW(hFont, sizeof(g_lastFont), &g_lastFont) == 0) return false;
+    return (g_lastFont.lfEscapement != 0 || g_lastFont.lfOrientation != 0);
+}
+
+using ExtTextOutW_t = BOOL(WINAPI*)(HDC, int, int, UINT, const RECT*, LPCWSTR, UINT, const INT*);
+ExtTextOutW_t ExtTextOutW_Original = nullptr;
+
+BOOL WINAPI ExtTextOutW_Hook(HDC hdc, int x, int y, UINT options, 
+                              const RECT* lprect, LPCWSTR lpString, 
+                              UINT c, const INT* lpDx) {
+    int len = (int)c;
+    
+    if (LooksLikeLayoutText(lpString, len) && IsRotatedFont(hdc)) {
+        wcsncpy_s(g_lastText, lpString, len);
+        g_lastText[len] = 0;
+        g_captureTime = GetTickCount();
+        g_drawn = false;
+        return TRUE;
+    }
+
+    return ExtTextOutW_Original(hdc, x, y, options, lprect, lpString, c, lpDx);
+}
+
+using BitBlt_t = BOOL(WINAPI*)(HDC, int, int, int, int, HDC, int, int, DWORD);
+BitBlt_t BitBlt_Original = nullptr;
+
+BOOL WINAPI BitBlt_Hook(HDC hdcDest, int xDest, int yDest, int width, int height,
+                         HDC hdcSrc, int xSrc, int ySrc, DWORD rop) {
+    
+    BOOL result = BitBlt_Original(hdcDest, xDest, yDest, width, height, hdcSrc, xSrc, ySrc, rop);
+    
+    DWORD elapsed = GetTickCount() - g_captureTime;
+    
+    if (g_lastText[0] != 0 && elapsed < 100 && !g_drawn) {
+        if (width >= 30 && width <= 50 && height >= 15 && height <= 30) {
+            
+            RECT rc = { xDest, yDest, xDest + width, yDest + height };
+            
+            HBRUSH hBrush = GetSysColorBrush(COLOR_3DFACE);
+            FillRect(hdcDest, &rc, hBrush);
+            
+            LOGFONTW lf = g_lastFont;
+            lf.lfEscapement = 0;
+            lf.lfOrientation = 0;
+            
+            HFONT hFont = CreateFontIndirectW(&lf);
+            if (hFont) {
+                HFONT hOld = (HFONT)SelectObject(hdcDest, hFont);
+                SetBkMode(hdcDest, TRANSPARENT);
+                SetTextColor(hdcDest, GetSysColor(COLOR_BTNTEXT));
+                
+                DrawTextW(hdcDest, g_lastText, -1, &rc, 
+                          DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+                
+                SelectObject(hdcDest, hOld);
+                DeleteObject(hFont);
+            }
+            
+            g_drawn = true;
+        }
+    }
+    
+    return result;
+}
+
+BOOL Wh_ModInit() {
+    Wh_SetFunctionHook((void*)ExtTextOutW, (void*)ExtTextOutW_Hook, (void**)&ExtTextOutW_Original);
+    Wh_SetFunctionHook((void*)BitBlt, (void*)BitBlt_Hook, (void**)&BitBlt_Original);
+    return TRUE;
+}
+
