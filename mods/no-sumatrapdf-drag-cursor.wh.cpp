4404178aff14e93b1dfed2cdcbc7dd8a63a4cb6e
ItsProfesssional <63961221+ItsProfessional@users.noreply.github.com>
2026-03-10 11:28:26 +0000

    Add No SumatraPDF Drag Cursor mod

---
+++ mods/no-sumatrapdf-drag-cursor.wh.cpp
@@ -0,0 +1,73 @@
+// ==WindhawkMod==
+// @id              no-sumatrapdf-drag-cursor
+// @name            No SumatraPDF Drag Cursor
+// @description     Remove the drag cursor which is shown when panning a document using by dragging with the cursor.
+// @version         1.0.0
+// @author          ItsProfessional
+// @github          https://github.com/ItsProfessional
+// @include         SumatraPDF.exe
+// ==/WindhawkMod==
+
+// Source code is published under The GNU General Public License v3.0.
+
+// ==WindhawkModReadme==
+/*
+# No SumatraPDF drag cursor
+This mod removes the drag cursor which is shown when panning a document using by dragging with the cursor.
+![SumatraPDF Drag Cursor](https://raw.githubusercontent.com/ItsProfessional/Screenshots/refs/heads/main/Windhawk/no-sumatrapdf-drag-cursor.png)
+*/
+// ==/WindhawkModReadme==
+
+#include <Windows.h>
+
+#ifdef _WIN64
+#define STDCALL  __cdecl
+#define SSTDCALL L"__cdecl"
+#else
+#define STDCALL  __stdcall
+#define SSTDCALL L"__stdcall"
+#endif
+
+HCURSOR g_hCursor{0};
+
+using LoadCursor_t = decltype(&LoadCursor);
+LoadCursor_t LoadCursor_orig;
+HCURSOR STDCALL LoadCursor_hook(HINSTANCE hInstance, LPCWSTR lpCursorName) {
+    HCURSOR hCursor = LoadCursor_orig(hInstance, lpCursorName);
+    
+    if(!g_hCursor && IS_INTRESOURCE(lpCursorName) && (ULONG_PTR)lpCursorName == 132)
+        g_hCursor = hCursor;
+
+    return hCursor;
+}
+
+using SetCursor_t = decltype(&SetCursor);
+SetCursor_t SetCursor_orig;
+HCURSOR STDCALL SetCursor_hook(HCURSOR hCursor) {
+    if(hCursor == g_hCursor) return GetCursor();
+
+    return SetCursor_orig(hCursor);
+}
+
+BOOL Wh_ModInit(void)
+{
+    Wh_Log(L">");
+
+    if (!Wh_SetFunctionHook((void*)LoadCursor, (void*)LoadCursor_hook, (void**)&LoadCursor_orig))
+    {
+        Wh_Log(L"Failed to hook LoadCursor");
+        return FALSE; 
+    }
+
+    if (!Wh_SetFunctionHook((void*)SetCursor, (void*)SetCursor_hook, (void**)&SetCursor_orig))
+    {
+        Wh_Log(L"Failed to hook SetCursor");
+        return FALSE; 
+    }
+
+    return TRUE;
+}
+
+void Wh_ModUninit() {
+    Wh_Log(L">");
+}
