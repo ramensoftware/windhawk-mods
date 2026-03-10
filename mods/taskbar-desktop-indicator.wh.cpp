837ce3ff69fa4c7be66e9fbfef982d1d3d712f87
simon-ami <102378134+simon-ami@users.noreply.github.com>
2026-03-10 11:18:08 +0100

    Bump taskbar desktop indicator version to 1.2.1

--- mods/taskbar-desktop-indicator.wh.cpp
+++ mods/taskbar-desktop-indicator.wh.cpp
@@ -2,7 +2,7 @@
 // @id              taskbar-desktop-indicator
 // @name            Taskbar Desktop Indicator
 // @description     Displays the current virtual desktop as a number or marker in the Windows 11 taskbar clock area
-// @version         1.2.0
+// @version         1.2.1
 // @author          Simon Benedict
 // @github          https://github.com/simon-ami
 // @include         explorer.exe
