212640aafbbfe39f8cc337882ec7df26ca60064c
Louis047 <zerokineticzk@gmail.com>
2026-03-30 21:00:54 +0530

    changed version to base

--- mods/auto-custom-titlebar-colors.cpp
+++ mods/auto-custom-titlebar-colors.cpp
@@ -2,7 +2,7 @@
 // @id              auto-custom-titlebar-colours
 // @name            Auto Custom Titlebar Colors
 // @description     Auto-switches titlebar dark/light mode with the Windows theme, with separate custom colours for active/inactive windows in both modes
-// @version         1.1.0
+// @version         1.0.0
 // @author          Lone
 // @github			https://github.com/Louis047
 // @include         *
