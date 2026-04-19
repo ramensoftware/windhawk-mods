f2199ab55ed1a58092ebd8b18cec48fede18b34b
sebi <sebisebpaw@gmail.com>
2026-04-19 13:39:57 -0500

    Add pid filter to prevent modifying windows belonging to other electron apps
    Hook ShowWindow to catch windows created after mod injection
    
    Skip popup windows to create breaking tray context menu

--- mods/resizable-discord.wh.cpp
+++ mods/resizable-discord.wh.cpp
@@ -2,7 +2,7 @@
 // @id              resizable-discord
 // @name            Discord Transparency Resize Fix
 // @description     Forces discord to be resizable with vencord window transparency
-// @version         1.0.0
+// @version         1.0.1
 // @author          gameknight963
 // @include         discord.exe
 // @github          https://github.com/Gameknight963
@@ -12,7 +12,7 @@
 /*
 This mod forces resizing on Discord when using window transparency + native titlebar.
 
-How it works: It enables WS_THICKFRAME for proper resizing behavior,
+How it works: It enables WS_THICKFRAME by hooking ,
 which Discord disables because it uses a custom titlebar, and Electron handles
 the resize logic through software in this custom titlebar mode.
 
@@ -24,21 +24,31 @@ the window is not resizable. This mod fixes that.
 
 #include <windows.h>
 
+typedef BOOL(WINAPI* ShowWindow_t)(HWND, int);
+ShowWindow_t ShowWindow_Original;
+
 void HookWindow(HWND hWnd)
 {
-    wchar_t className[256];
-    GetClassNameW(hWnd, className, 256);
-    if (wcscmp(className, L"Chrome_WidgetWin_1") != 0)
-        return;
+    DWORD pid;
+    GetWindowThreadProcessId(hWnd, &pid);
+    if (pid != GetCurrentProcessId()) return;
 
     LONG_PTR style = GetWindowLongPtr(hWnd, GWL_STYLE);
+    if (style & WS_POPUP)
+        return;
     SetWindowLongPtr(hWnd, GWL_STYLE, style | WS_THICKFRAME);
+}
+
+BOOL WINAPI ShowWindow_Hook(HWND hWnd, int nCmdShow) {
+    HookWindow(hWnd);
+    return ShowWindow_Original(hWnd, nCmdShow);
 }
 
 BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM) { HookWindow(hWnd); return TRUE; }
 
 BOOL Wh_ModInit()
 {
+    Wh_SetFunctionHook((void*)ShowWindow, (void*)ShowWindow_Hook, (void**)&ShowWindow_Original);
     EnumWindows(EnumWindowsProc, 0);
     return TRUE;
 }
