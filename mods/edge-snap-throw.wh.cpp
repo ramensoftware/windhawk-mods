286bedcb0c1fdcc6bed9a9a06d15f540bff45a0c
Getrektbynoob15 <109096888+Getrektbynoob15@users.noreply.github.com>
2026-04-22 11:40:42 +0200

    Fix GitHub link in edge-snap-throw.wh.cpp

--- mods/edge-snap-throw.wh.cpp
+++ mods/edge-snap-throw.wh.cpp
@@ -4,7 +4,7 @@
 // @description     Slide a window into a screen edge and it snaps like native Windows snap
 // @version         2.0.1
 // @author          Getrektbynoob15
-// @github          https://github.com/getrektbynoob20
+// @github          https://github.com/getrektbynoob15
 // @include         *
 // @compilerOptions -lcomctl32 -ldwmapi
 // ==/WindhawkMod==
