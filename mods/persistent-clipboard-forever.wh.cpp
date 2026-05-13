6717361b3854f12ab3f244aec84e39de0199e3ac
Guy <aahwjxn@gmail.com>
2026-05-13 15:42:45 +0300

    Update persistent-clipboard-forever.wh.cpp

--- mods/persistent-clipboard-forever.wh.cpp
+++ mods/persistent-clipboard-forever.wh.cpp
@@ -1,5 +1,5 @@
 // ==WindhawkMod==
-// @id              mods/clipboard-forever.wh.cpp
+// @id              persistent-clipboard-forever
 // @name            Clipboard Forever
 // @description     Persist clipboard text/image history and restore it after clipboard clears or Windows restarts
 // @version         1.2.0
