94f1db1df47831199ce4e41b579e8a3c93cb5392
Rod Boev <rod.boev@gmail.com>
2026-05-15 14:55:44 -0400

    Add ExplorerPatcher Taskbar Button Width mod

---
+++ mods/ep-taskbar-button-width.wh.cpp
@@ -0,0 +1,179 @@
+// ==WindhawkMod==
+// @id              ep-taskbar-button-width
+// @name            ExplorerPatcher Taskbar Button Width
+// @description     Customize the minimum width of taskbar buttons when using ExplorerPatcher's Windows 10 taskbar
+// @version         1.0.0
+// @author          Rod Boev
+// @github          https://github.com/rodboev
+// @include         explorer.exe
+// @architecture    x86-64
+// ==/WindhawkMod==
+
+// ==WindhawkModReadme==
+/*
+# ExplorerPatcher Taskbar Button Width
+
+The standard MinWidth registry hack doesn't work with ExplorerPatcher's
+Windows 10 taskbar. This mod lets you adjust button width as a percentage
+(default 150%).
+
+Requires ExplorerPatcher with Windows 10 taskbar enabled.
+*/
+// ==/WindhawkModReadme==
+
+// ==WindhawkModSettings==
+/*
+- buttonWidthPercent: 150
+  $name: Button width (% of default)
+  $description: >-
+    Percentage to scale button width. 100% = no change, 150% = 50% wider.
+    Range: 50-300.
+*/
+// ==/WindhawkModSettings==
+
+#include <psapi.h>
+#include <atomic>
+
+struct {
+    int buttonWidthPercent;
+} g_settings;
+
+std::atomic<bool> g_hooked{false};
+
+using ComputeSingleButtonWidth_t = int(WINAPI*)(void* pThis, int groupType, void* pTaskBtnGroup, int* pWidth);
+ComputeSingleButtonWidth_t ComputeSingleButtonWidth_Original;
+
+using LoadLibraryExW_t = decltype(&LoadLibraryExW);
+LoadLibraryExW_t LoadLibraryExW_Original;
+
+int WINAPI ComputeSingleButtonWidth_Hook(void* pThis, int groupType, void* pTaskBtnGroup, int* pWidth) {
+    int result = ComputeSingleButtonWidth_Original(pThis, groupType, pTaskBtnGroup, pWidth);
+
+    if (g_settings.buttonWidthPercent != 100) {
+        result = (result * g_settings.buttonWidthPercent) / 100;
+        if (pWidth) {
+            *pWidth = (*pWidth * g_settings.buttonWidthPercent) / 100;
+        }
+    }
+
+    return result;
+}
+
+void RefreshTaskbar() {
+    // Primary taskbar: Shell_TrayWnd -> ReBarWindow32 -> MSTaskSwWClass
+    HWND taskbar = FindWindow(L"Shell_TrayWnd", nullptr);
+    if (taskbar) {
+        HWND hReBarWindow32 = FindWindowEx(taskbar, nullptr, L"ReBarWindow32", nullptr);
+        if (hReBarWindow32) {
+            HWND hMSTaskSwWClass = FindWindowEx(hReBarWindow32, nullptr, L"MSTaskSwWClass", nullptr);
+            if (hMSTaskSwWClass) {
+                SendMessage(hMSTaskSwWClass, 0x452, 3, 0);
+            }
+        }
+    }
+
+    // Secondary taskbars: Shell_SecondaryTrayWnd -> WorkerW -> MSTaskSwWClass
+    HWND secondaryTaskbar = nullptr;
+    while ((secondaryTaskbar = FindWindowEx(nullptr, secondaryTaskbar, L"Shell_SecondaryTrayWnd", nullptr)) != nullptr) {
+        HWND hWorkerW = FindWindowEx(secondaryTaskbar, nullptr, L"WorkerW", nullptr);
+        if (hWorkerW) {
+            HWND hMSTaskSwWClass = FindWindowEx(hWorkerW, nullptr, L"MSTaskSwWClass", nullptr);
+            if (hMSTaskSwWClass) {
+                SendMessage(hMSTaskSwWClass, 0x452, 3, 0);
+            }
+        }
+    }
+}
+
+bool IsExplorerPatcherModule(HMODULE module) {
+    WCHAR path[MAX_PATH];
+    if (!GetModuleFileName(module, path, ARRAYSIZE(path))) {
+        return false;
+    }
+    PCWSTR name = wcsrchr(path, L'\\');
+    if (!name) return false;
+    name++;
+    return _wcsnicmp(L"ep_taskbar.", name, 11) == 0;
+}
+
+bool HookExplorerPatcher(HMODULE module, bool calledFromInit) {
+    if (g_hooked.exchange(true)) {
+        return true;
+    }
+
+    void* ptr = (void*)GetProcAddress(module,
+        "?_ComputeSingleButtonWidth@CTaskListWnd@@IEAAHW4eTBGROUPTYPE@@PEAUITaskBtnGroup@@PEAH@Z");
+
+    if (!ptr) {
+        Wh_Log(L"ERROR: _ComputeSingleButtonWidth not found");
+        g_hooked = false;
+        return false;
+    }
+
+    Wh_SetFunctionHook(ptr, (void*)ComputeSingleButtonWidth_Hook, (void**)&ComputeSingleButtonWidth_Original);
+
+    if (!calledFromInit) {
+        Wh_ApplyHookOperations();
+        RefreshTaskbar();
+    }
+
+    Wh_Log(L"Hooked at %p (scale: %d%%)", ptr, g_settings.buttonWidthPercent);
+    return true;
+}
+
+HMODULE WINAPI LoadLibraryExW_Hook(LPCWSTR lpLibFileName, HANDLE hFile, DWORD dwFlags) {
+    HMODULE module = LoadLibraryExW_Original(lpLibFileName, hFile, dwFlags);
+    if (module && !g_hooked && IsExplorerPatcherModule(module)) {
+        Wh_Log(L"ExplorerPatcher loaded: %s", lpLibFileName);
+        HookExplorerPatcher(module, false);
+    }
+    return module;
+}
+
+void LoadSettings() {
+    g_settings.buttonWidthPercent = Wh_GetIntSetting(L"buttonWidthPercent");
+    if (g_settings.buttonWidthPercent < 50) g_settings.buttonWidthPercent = 50;
+    if (g_settings.buttonWidthPercent > 300) g_settings.buttonWidthPercent = 300;
+}
+
+BOOL Wh_ModInit() {
+    Wh_Log(L"=== EP Taskbar Button Width v1.3.0 ===");
+    LoadSettings();
+
+    HMODULE hMods[1024];
+    DWORD cbNeeded;
+    if (EnumProcessModules(GetCurrentProcess(), hMods, sizeof(hMods), &cbNeeded)) {
+        for (size_t i = 0; i < cbNeeded / sizeof(HMODULE); i++) {
+            if (IsExplorerPatcherModule(hMods[i])) {
+                HookExplorerPatcher(hMods[i], true);
+                break;
+            }
+        }
+    }
+
+    HMODULE kb = GetModuleHandle(L"kernelbase.dll");
+    if (kb) {
+        auto pLoadLib = (LoadLibraryExW_t)GetProcAddress(kb, "LoadLibraryExW");
+        if (pLoadLib) {
+            Wh_SetFunctionHook((void*)pLoadLib, (void*)LoadLibraryExW_Hook, (void**)&LoadLibraryExW_Original);
+        }
+    }
+
+    return TRUE;
+}
+
+void Wh_ModAfterInit() {
+    if (g_hooked) {
+        RefreshTaskbar();
+    }
+}
+
+void Wh_ModUninit() {
+    g_settings.buttonWidthPercent = 100;
+    RefreshTaskbar();
+}
+
+void Wh_ModSettingsChanged() {
+    LoadSettings();
+    RefreshTaskbar();
+}
