c99b3afa1747ebfd2d50315f8c78d95916a3f445
Anixx <neptunia@mail.ru>
2026-05-20 20:59:53 +0300

    Change screenshot URL in native-titlebars mod
    Updated the screenshot URL in the mod readme.

--- mods/native-titlebars-uwp-lite.wh.cpp
+++ mods/native-titlebars-uwp-lite.wh.cpp
@@ -18,7 +18,7 @@ Replaces the UWP titlebars with native Win32 titlebars.
 
 This mod is focused on the Classic theme, so may produce sub-optimal results in other cases.
 
-![Screenshot](https://cdn.phototourl.com/free/2026-05-20-82d53360-4a25-42fa-b349-0d32f828e9bc.png)
+![Screenshot](https://i.imgur.com/gqw78kB.png)
 
 */
 // ==/WindhawkModReadme==
