e932e4d8105ab3a8470835b69d3181e12fd589a4
akamienski <kamienski.adam@gmail.com>
2026-05-26 16:57:28 +0200

    Fix image link for horizontal rounded thumbnails

--- mods/simple-window-switcher.wh.cpp
+++ mods/simple-window-switcher.wh.cpp
@@ -61,7 +61,7 @@ Additional improvements made by [Asteski](https://github.com/Asteski).
 
 *Horizontal rounded with thumbnails and no icons*
 
-![Horizontal rounded with thumbnails and no icons]https://github.com/Asteski/Windhawk-Mods/blob/cc72179d1ff603f3d3cf94fd41230ebea774bc8b/img/simple-window-switcher/8.png)
+![Horizontal rounded with thumbnails and no icons](https://raw.githubusercontent.com/Asteski/Windhawk-Mods/refs/heads/main/img/simple-window-switcher/8.png)
 
 */
 // ==/WindhawkModReadme==
