c50522a37ec829a99e12fe12e4f106389b0369f9
Anixx <neptunia@mail.ru>
2026-06-16 15:55:00 +0300

    Add a fix for Internet Download Manager

--- mods/classic-theme-transparency-fix.wh.cpp
+++ mods/classic-theme-transparency-fix.wh.cpp
@@ -2,25 +2,50 @@
 // @id              classic-theme-transparency-fix
 // @name            Classic theme transparency fix
 // @description     Fixes transparency glitches in Classic theme
-// @version         1.1
+// @version         1.2
 // @author          anixx
 // @github          https://github.com/Anixx
 // @include         *
 // @include         dllhost.exe
-
 // ==/WindhawkMod==
 
 // ==WindhawkModReadme==
 /*
-This mod includes a subset of functionality of the mod "DWM Unextend frames", but only
-includes fixes needed for Classic theme. It hooks less functions, is more lightweight, does not
-use pointer arithmetic and the code is more compact.
-It also injects dllhost.exe by default, which is needed for Windows Photo Viewer
-and some dialogs.
+This mod removes the unwanted transparency from windows under Classic theme.
+The option `Remove accent transparency from dialogs` is intended to fix the download dialog in
+Internet Download Manager and, maybe, other apps. If you experience black rectangles instead of
+shadiows, turn it off.
 */
 // ==/WindhawkModReadme==
 
+// ==WindhawkModSettings==
+/*
+- removeDialogTransparency: true
+  $name: Remove accent transparency from dialogs
+  $description: Removes accent transparency from newly created windows of class #32770
+*/
+// ==/WindhawkModSettings==
+
 #include <windhawk_utils.h>
+
+// --- Structs and types for dialog transparency removal ---
+
+struct ACCENT_POLICY {
+    DWORD AccentState;
+    DWORD AccentFlags;
+    DWORD GradientColor;
+    DWORD AnimationId;
+};
+
+struct WINDOWCOMPOSITIONATTRIBDATA {
+    DWORD  Attrib;
+    PVOID  pvData;
+    SIZE_T cbData;
+};
+
+typedef BOOL (WINAPI* pSetWindowCompositionAttribute)(HWND, WINDOWCOMPOSITIONATTRIBDATA*);
+
+// --- DWM / composition hooks ---
 
 typedef HRESULT (WINAPI *DwmIsCompositionEnabled_t)(BOOL *);
 DwmIsCompositionEnabled_t DwmIsCompositionEnabled_orig;
@@ -37,13 +62,44 @@ BOOL WINAPI IsCompositionActive_hook()
     return FALSE;
 }
 
+// --- ShowWindow hook for dialog transparency removal ---
+
+using ShowWindow_t = BOOL(WINAPI*)(HWND, int);
+ShowWindow_t ShowWindow_Original;
+
+BOOL WINAPI ShowWindow_Hook(HWND hWnd, int nCmdShow)
+{
+    if (nCmdShow != SW_HIDE && hWnd) {
+        WCHAR className[64];
+        if (GetClassName(hWnd, className, 64) && !wcscmp(className, L"#32770")) {
+            auto SetWCA = (pSetWindowCompositionAttribute)GetProcAddress(
+                GetModuleHandle(L"user32.dll"), "SetWindowCompositionAttribute");
+
+            if (SetWCA) {
+                ACCENT_POLICY accent = {1};
+                WINDOWCOMPOSITIONATTRIBDATA data = {19, &accent, sizeof(accent)};
+                SetWCA(hWnd, &data);
+            }
+        }
+    }
+    return ShowWindow_Original(hWnd, nCmdShow);
+}
+
+// --- Mod init ---
+
 BOOL Wh_ModInit()
 {
     HMODULE dwmapiModule = LoadLibraryExW(L"dwmapi.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
     FARPROC pFunction = GetProcAddress(dwmapiModule, "DwmIsCompositionEnabled");
     WindhawkUtils::Wh_SetFunctionHookT((void*)pFunction, (void*)DwmIsCompositionEnabled_hook, (void**)&DwmIsCompositionEnabled_orig);
+
     HMODULE uxthemeModule = LoadLibraryExW(L"uxtheme.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
     pFunction = GetProcAddress(uxthemeModule, "IsCompositionActive");
     WindhawkUtils::Wh_SetFunctionHookT((void*)pFunction, (void*)IsCompositionActive_hook, (void**)&IsCompositionActive_orig);
+
+    if (Wh_GetIntSetting(L"removeDialogTransparency")) {
+        Wh_SetFunctionHook((void*)ShowWindow, (void*)ShowWindow_Hook, (void**)&ShowWindow_Original);
+    }
+
     return TRUE;
 }
