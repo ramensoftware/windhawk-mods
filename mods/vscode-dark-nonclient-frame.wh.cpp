468ce161741f6ddd26078a7ec194df6f67683d61
v1b3s0 <146864395+v1b3s0@users.noreply.github.com>
2026-06-18 12:57:16 +0400

    Fix screenshot order and bump version

--- mods/vscode-dark-nonclient-frame.wh.cpp
+++ mods/vscode-dark-nonclient-frame.wh.cpp
@@ -2,7 +2,7 @@
 // @id              vscode-dark-nonclient-frame
 // @name            VS Code Dark Non-client Frame
 // @description     Forces VS Code's non-client frame into dark mode to remove the light 2px bottom edge in Windows light mode.
-// @version         1.0
+// @version         1.1
 // @author          v1b3s0
 // @github          https://github.com/v1b3s0
 // @license         MIT
