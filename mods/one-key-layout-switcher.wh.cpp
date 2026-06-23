689cba3d609f9f84bb3d60b0a9f17813fcbcd46f
Anixx <neptunia@mail.ru>
2026-06-24 00:31:39 +0300

    Update one-key-layout-switcher.wh.cpp

--- mods/one-key-layout-switcher.wh.cpp
+++ mods/one-key-layout-switcher.wh.cpp
@@ -21,7 +21,7 @@ and make sure that `dwm.exe` is in the list.
 
 // ==WindhawkModSettings==
 /*
-- switchMethod: altshift
+- switchMethod: both
   $name: Switch method
   $description: Choose which key combination to send for layout switching
   $options:
