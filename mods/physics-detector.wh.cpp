84c91824e5a186d8a3a309d6764801e0016ca253
Zicronium <prashantrajjha3@gmail.com>
2026-08-02 14:40:59 +0530

    Update physics-detector.wh.cpp

--- mods/physics-detector.wh.cpp
+++ mods/physics-detector.wh.cpp
@@ -3,7 +3,7 @@
 // @name            Physics for Panels - Detector
 // @description     Part 1 of 2: Detects taskbar auto-hide states and overlays it like a search menu when hovered.
 // @version         1.0.0
-// @author          Zicronix
+// @author          Zicronium
 // @include         explorer.exe
 // @architecture    x86-64
 // @compilerOptions -luser32 -lkernel32 -lshell32
