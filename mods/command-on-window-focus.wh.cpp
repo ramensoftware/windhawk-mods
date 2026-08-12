3e58fbaa1dedf6ddf4822a6424411c18e51bd5f9
rom4ster <rohansampat97@gmail.com>
2026-08-12 01:40:03 -0500

    Set timeouts, update readme, and turn into a tool mod

--- mods/command-on-window-focus.wh.cpp
+++ mods/command-on-window-focus.wh.cpp
@@ -5,37 +5,25 @@
 // @version         0.1
 // @author          rom4ster
 // @github          https://github.com/rom4ster
-// @include         explorer.exe
+// @include         windhawk.exe
 // @compilerOptions -lcomdlg32
 // @license         MIT
 // ==/WindhawkMod==
 
 // ==WindhawkModReadme==
 /*
-# Your Awesome Mod
-This is a place for useful information about your mod. Use it to describe the
-mod, explain why it's useful, and add any other relevant details. You can use
-[Markdown](https://en.wikipedia.org/wiki/Markdown) to add links and
-**formatting** to the readme.
-
-This short sample customizes Microsoft Paint by forcing it to use just a single
-color, and by blocking file opening. To see the mod in action:
-- Compile the mod with the button on the left or with Ctrl+B.
-- Run Microsoft Paint from the start menu (type "Paint") or by running
-  mspaint.exe.
-- Draw something and notice that the orange color is always used, regardless of
-  the color you pick.
-- Try opening a file and notice that it's blocked.
-
-# Getting started
-Check out the documentation
-[here](https://github.com/ramensoftware/windhawk/wiki/Creating-a-new-mod).
+# Command On Window Focus
+This mod intercepts a window focus and will run any command on window focus. 
+This mod has a filter which lets you specify which windows will be allowed to have focus based on the path of the executable
+running them. There is also the option of alternate commands which will only run if none of the filters match.
+An example use case for me personally is running a script that sets the scroll wheel on my mouse to a different scroll type every time 
+I focus a game (and the alternate command will reset it to the original type). 
 */
 // ==/WindhawkModReadme==
 
 // ==WindhawkModSettings==
 /*
-- filters: [C:\\Program Files\\*]
+- filters: [C:\\Program Files\\.*]
   $name: Path Filters
   $description: Paths to match against
 - cmd: echo hi
@@ -50,16 +38,13 @@ Check out the documentation
 */
 // ==/WindhawkModSettings==
 
-// The source code of the mod starts here. This sample was inspired by the great
-// article of Kyle Halladay, X64 Function Hooking by Example:
-// https://kylehalladay.com/blog/2020/11/13/Hooking-By-Example.html
-// If you're new to terms such as code injection and function hooking, the
-// article is great to get started.
-
 #include <gdiplus.h>
 #include <regex>
 #include <string>
 
+#define Init WhTool_ModInit 
+#define SettingsChanged WhTool_ModSettingsChanged 
+#define Uninit WhTool_ModUninit  
 
 using namespace Gdiplus;
 
@@ -155,10 +140,13 @@ void CALLBACK WinEventProc(
     // 3. Get its executable path
     WCHAR exePath[MAX_PATH];
     DWORD exePathSize = ARRAYSIZE(exePath);
-    if (QueryFullProcessImageNameW(hProcess, 0, exePath, &exePathSize)) {
+    BOOL processPath = QueryFullProcessImageNameW(hProcess, 0, exePath, &exePathSize);
+    CloseHandle(hProcess);
+    if (processPath) {
         Wh_Log(L"Foreground window (PID %lu) launched from: %s", processId, exePath);
     } else {
         Wh_Log(L"Failed to get process path, error: %lu", GetLastError());
+        return;
     }
     bool match = false;
     
@@ -230,7 +218,7 @@ DWORD WINAPI HookThreadProc(LPVOID)
 
 // The mod is being initialized, load settings, hook functions, and do other
 // initialization stuff if required.
-BOOL Wh_ModInit() {
+BOOL Init() {
     Wh_Log(L"Init");
     LoadSettings();
 
@@ -245,19 +233,198 @@ BOOL Wh_ModInit() {
 
 
 // The mod is being unloaded, free all allocated resources.
-void Wh_ModUninit() {
+void Uninit() {
     Wh_Log(L"Uninit");
     if (g_hThread) {
         PostThreadMessage(g_threadId, WM_QUIT, 0, 0);
-        WaitForSingleObject(g_hThread, INFINITE);
+        WaitForSingleObject(g_hThread, 10000);
         CloseHandle(g_hThread);
         g_hThread = nullptr;
     }
 }
 
 // The mod setting were changed, reload them.
-void Wh_ModSettingsChanged() {
+void SettingsChanged() {
     Wh_Log(L"SettingsChanged");
 
     LoadSettings();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Windhawk tool mod implementation for mods which don't need to inject to other
+// processes or hook other functions. Context:
+// https://github.com/ramensoftware/windhawk/wiki/Mods-as-tools:-Running-mods-in-a-dedicated-process
+//
+// The mod will load and run in a dedicated windhawk.exe process.
+//
+// Paste the code below as part of the mod code, and use these callbacks:
+// * WhTool_ModInit
+// * WhTool_ModSettingsChanged
+// * WhTool_ModUninit
+//
+// Currently, other callbacks are not supported.
+
+bool g_isToolModProcessLauncher;
+HANDLE g_toolModProcessMutex;
+
+void WINAPI EntryPoint_Hook() {
+    Wh_Log(L">");
+    ExitThread(0);
+}
+
+BOOL Wh_ModInit() {
+    DWORD sessionId;
+    if (ProcessIdToSessionId(GetCurrentProcessId(), &sessionId) &&
+        sessionId == 0) {
+        return FALSE;
+    }
+
+    bool isExcluded = false;
+    bool isToolModProcess = false;
+    bool isCurrentToolModProcess = false;
+    int argc;
+    LPWSTR* argv = CommandLineToArgvW(GetCommandLine(), &argc);
+    if (!argv) {
+        Wh_Log(L"CommandLineToArgvW failed");
+        return FALSE;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (wcscmp(argv[i], L"-service") == 0 ||
+            wcscmp(argv[i], L"-service-start") == 0 ||
+            wcscmp(argv[i], L"-service-stop") == 0) {
+            isExcluded = true;
+            break;
+        }
+    }
+
+    for (int i = 1; i < argc - 1; i++) {
+        if (wcscmp(argv[i], L"-tool-mod") == 0) {
+            isToolModProcess = true;
+            if (wcscmp(argv[i + 1], WH_MOD_ID) == 0) {
+                isCurrentToolModProcess = true;
+            }
+            break;
+        }
+    }
+
+    LocalFree(argv);
+
+    if (isExcluded) {
+        return FALSE;
+    }
+
+    if (isCurrentToolModProcess) {
+        g_toolModProcessMutex =
+            CreateMutex(nullptr, TRUE, L"windhawk-tool-mod_" WH_MOD_ID);
+        if (!g_toolModProcessMutex) {
+            Wh_Log(L"CreateMutex failed");
+            ExitProcess(1);
+        }
+
+        if (GetLastError() == ERROR_ALREADY_EXISTS) {
+            Wh_Log(L"Tool mod already running (%s)", WH_MOD_ID);
+            ExitProcess(1);
+        }
+
+        if (!WhTool_ModInit()) {
+            ExitProcess(1);
+        }
+
+        IMAGE_DOS_HEADER* dosHeader =
+            (IMAGE_DOS_HEADER*)GetModuleHandle(nullptr);
+        IMAGE_NT_HEADERS* ntHeaders =
+            (IMAGE_NT_HEADERS*)((BYTE*)dosHeader + dosHeader->e_lfanew);
+
+        DWORD entryPointRVA = ntHeaders->OptionalHeader.AddressOfEntryPoint;
+        void* entryPoint = (BYTE*)dosHeader + entryPointRVA;
+
+        Wh_SetFunctionHook(entryPoint, (void*)EntryPoint_Hook, nullptr);
+        return TRUE;
+    }
+
+    if (isToolModProcess) {
+        return FALSE;
+    }
+
+    g_isToolModProcessLauncher = true;
+    return TRUE;
+}
+
+void Wh_ModAfterInit() {
+    if (!g_isToolModProcessLauncher) {
+        return;
+    }
+
+    WCHAR currentProcessPath[MAX_PATH];
+    switch (GetModuleFileName(nullptr, currentProcessPath,
+                              ARRAYSIZE(currentProcessPath))) {
+        case 0:
+        case ARRAYSIZE(currentProcessPath):
+            Wh_Log(L"GetModuleFileName failed");
+            return;
+    }
+
+    WCHAR
+    commandLine[MAX_PATH + 2 +
+                (sizeof(L" -tool-mod \"" WH_MOD_ID "\"") / sizeof(WCHAR)) - 1];
+    swprintf_s(commandLine, L"\"%s\" -tool-mod \"%s\"", currentProcessPath,
+               WH_MOD_ID);
+
+    HMODULE kernelModule = GetModuleHandle(L"kernelbase.dll");
+    if (!kernelModule) {
+        kernelModule = GetModuleHandle(L"kernel32.dll");
+        if (!kernelModule) {
+            Wh_Log(L"No kernelbase.dll/kernel32.dll");
+            return;
+        }
+    }
+
+    using CreateProcessInternalW_t = BOOL(WINAPI*)(
+        HANDLE hUserToken, LPCWSTR lpApplicationName, LPWSTR lpCommandLine,
+        LPSECURITY_ATTRIBUTES lpProcessAttributes,
+        LPSECURITY_ATTRIBUTES lpThreadAttributes, WINBOOL bInheritHandles,
+        DWORD dwCreationFlags, LPVOID lpEnvironment, LPCWSTR lpCurrentDirectory,
+        LPSTARTUPINFOW lpStartupInfo,
+        LPPROCESS_INFORMATION lpProcessInformation,
+        PHANDLE hRestrictedUserToken);
+    CreateProcessInternalW_t pCreateProcessInternalW =
+        (CreateProcessInternalW_t)GetProcAddress(kernelModule,
+                                                 "CreateProcessInternalW");
+    if (!pCreateProcessInternalW) {
+        Wh_Log(L"No CreateProcessInternalW");
+        return;
+    }
+
+    STARTUPINFO si{
+        .cb = sizeof(STARTUPINFO),
+        .dwFlags = STARTF_FORCEOFFFEEDBACK,
+    };
+    PROCESS_INFORMATION pi;
+    if (!pCreateProcessInternalW(nullptr, currentProcessPath, commandLine,
+                                 nullptr, nullptr, FALSE, NORMAL_PRIORITY_CLASS,
+                                 nullptr, nullptr, &si, &pi, nullptr)) {
+        Wh_Log(L"CreateProcess failed");
+        return;
+    }
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+}
+
+void Wh_ModSettingsChanged() {
+    if (g_isToolModProcessLauncher) {
+        return;
+    }
+
+    WhTool_ModSettingsChanged();
+}
+
+void Wh_ModUninit() {
+    if (g_isToolModProcessLauncher) {
+        return;
+    }
+
+    WhTool_ModUninit();
+    ExitProcess(0);
 }
