f658ee33531fb949e11f59c2c4bc0ca9c535c688
Potassiumuncher <ishakalom017@gmail.com>
2026-08-23 18:51:09 +0100

    Update wobbly-windows.wh.cpp

--- mods/wobbly-windows.wh.cpp
+++ mods/wobbly-windows.wh.cpp
@@ -7,6 +7,7 @@
 // @github        https://github.com/Potassiumuncher
 // @include       *
 // @exclude       explorer.exe
+// @exclude       notepad.exe
 // @license       GPL-2.0-or-later
 // @compilerOptions -ldwmapi -lgdi32 -ld2d1 -luser32 -lcomctl32 -lwinmm
 // ==/WindhawkMod==
@@ -21,7 +22,7 @@
 Credit - KWin's Wobbly Windows for making this possible 
 
 ## Compatibility notes
-Dose not work as intended in File Explorer, excluded for the time being until its resolved.
+Dose not work as intended in File Explorer, notepad (winui) excluded for the time being until its resolved.
 
 Added a setting to change corner radius so it is compatable with "Custom Window Corner Radius" mod (the default is 8 *windows default)
 
