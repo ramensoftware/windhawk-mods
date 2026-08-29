fffe8e992faca018ea2fab5e1b71dd6934613876
didrmt1 <57950407+didrmt1@users.noreply.github.com>
2026-08-29 20:53:00 +0900

    Update win10-taskbar-volume-slider.wh.cpp

--- mods/win10-taskbar-volume-slider.wh.cpp
+++ mods/win10-taskbar-volume-slider.wh.cpp
@@ -23,7 +23,7 @@
 // ==WindhawkModReadme==
 /*
 # Windows 10 Taskbar Volume Slider
-<img width="548" height="331" alt="20260826_214734" src="https://github.com/user-attachments/assets/5368ad65-e470-4849-baae-577c7a81b81d" />
+<img width="548" height="331" alt="20260826_214734" src="https://raw.githubusercontent.com/didrmt1/windhawk-mods/main/mods/screenshot9.png" />
 Adds a permanently visible volume slider directly on the Windows 10 taskbar (left side of the system tray).
 
 ### Features
