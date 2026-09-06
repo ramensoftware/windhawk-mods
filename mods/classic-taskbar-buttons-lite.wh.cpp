de3b17f74104f939bb6a5327077d671e4a190761
Anixx <neptunia@mail.ru>
2026-09-06 20:47:53 +0300

    address ai criticism

--- mods/classic-taskbar-buttons-lite.wh.cpp
+++ mods/classic-taskbar-buttons-lite.wh.cpp
@@ -12,7 +12,7 @@
 // ==WindhawkModReadme==
 /*
 # Classic Taskbar 3D buttons
-Lightweight mod which restores 3D buttons on taskbar when using Windows Classic theme. 
+Lightweight mod which restores 3D buttons on taskbar when using Windows Classic theme.
 The idea is based on the mod by Aubymori (https://github.com/aubymori).
 
 Before:
@@ -23,7 +23,7 @@ After:
 
 ![After](https://i.imgur.com/Jz4EkRQ.png)
 
-Be warned that the progress indicator will not be displayed on the task buttons in the 3D mode, so if you need the progress bar, don't apply this mod. 
+Be warned that the progress indicator will not be displayed on the task buttons in the 3D mode, so if you need the progress bar, don't apply this mod.
 
 */
 // ==/WindhawkModReadme==
@@ -59,38 +59,74 @@ static HWND FindTaskListForTray(HWND hTray)
     return FindWindowExW(hTaskSw, NULL, L"MSTaskListWClass", NULL);
 }
 
+// Cache per taskbar thread to avoid window-manager lock in paint path.
+// Refreshes when tray rect changes, which covers ABN_POSCHANGED / WM_SETTINGCHANGE / resolution change.
+static thread_local HWND tl_hTray = NULL;
+static thread_local RECT tl_lastTrayRect = {};
+static thread_local bool tl_isHorizontal = true;
+static thread_local bool tl_cacheValid = false;
+
 void CALCON CTaskBtnGroup__DrawBar_hook(void *pThis, HDC hDC, void *pRenderInfo, PBUTTONRENDERINFOSTATES pRenderStates)
 {
     LPRECT lprcDest = (LPRECT)((char *)pRenderInfo + 4);
 
-    // Determine orientation by tray window this thread belongs to
-    DWORD curTid = GetCurrentThreadId();
     bool isHorizontal = true;
 
-    HWND hMain = FindWindowW(L"Shell_TrayWnd", NULL);
-    HWND hTrays[16];
-    int n = 0;
+    DWORD curTid = GetCurrentThreadId();
 
-    if (hMain)
-        hTrays[n++] = hMain;
-
-    HWND hSec = NULL;
-    while ((hSec = FindWindowExW(NULL, hSec, L"Shell_SecondaryTrayWnd", NULL)) && n < 16)
-        hTrays[n++] = hSec;
-
-    for (int i = 0; i < n; i++)
+    // Fast path: use cached tray geometry
+    if (tl_cacheValid && tl_hTray && IsWindow(tl_hTray))
     {
-        HWND hList = FindTaskListForTray(hTrays[i]);
-        if (!hList)
-            continue;
-
-        if (GetWindowThreadProcessId(hList, NULL)!= curTid)
-            continue;
-
         RECT rc;
-        GetWindowRect(hTrays[i], &rc);
-        isHorizontal = (rc.right - rc.left) > (rc.bottom - rc.top);
-        break;
+        if (GetWindowRect(tl_hTray, &rc))
+        {
+            if (memcmp(&rc, &tl_lastTrayRect, sizeof(RECT))!= 0)
+            {
+                tl_isHorizontal = (rc.right - rc.left) > (rc.bottom - rc.top);
+                tl_lastTrayRect = rc;
+            }
+            isHorizontal = tl_isHorizontal;
+        }
+        else
+        {
+            // GetWindowRect failed, keep last known value
+            isHorizontal = tl_isHorizontal;
+        }
+    }
+    else
+    {
+        // Slow path: resolve once
+        HWND hTrays[16];
+        int n = 0;
+
+        HWND hMain = FindWindowW(L"Shell_TrayWnd", NULL);
+        if (hMain)
+            hTrays[n++] = hMain;
+
+        HWND hSec = NULL;
+        while ((hSec = FindWindowExW(NULL, hSec, L"Shell_SecondaryTrayWnd", NULL)) && n < 16)
+            hTrays[n++] = hSec;
+
+        for (int i = 0; i < n; i++)
+        {
+            HWND hList = FindTaskListForTray(hTrays[i]);
+            if (!hList)
+                continue;
+
+            if (GetWindowThreadProcessId(hList, NULL)!= curTid)
+                continue;
+
+            RECT rc;
+            GetWindowRect(hTrays[i], &rc);
+            tl_hTray = hTrays[i];
+            tl_lastTrayRect = rc;
+            tl_isHorizontal = (rc.right - rc.left) > (rc.bottom - rc.top);
+            tl_cacheValid = true;
+            isHorizontal = tl_isHorizontal;
+            break;
+        }
+        // if not found, keep default horizontal and don't mark cache valid
+        // so we will retry next time
     }
 
     // Visual gap only, no layout shift
@@ -119,15 +155,6 @@ void CALCON CTaskBtnGroup__DrawBar_hook(void *pThis, HDC hDC, void *pRenderInfo,
     }
 }
 
-typedef long (*CTaskBtnGroup_SetLocation_t)(void *, int, int, LPRECT);
-CTaskBtnGroup_SetLocation_t CTaskBtnGroup_SetLocation_orig;
-
-long __cdecl CTaskBtnGroup_SetLocation_hook(void *pThis, int i1, int i2, LPRECT lprc)
-{
-    // Do not cut here - it causes staircase
-    return CTaskBtnGroup_SetLocation_orig(pThis, i1, i2, lprc);
-}
-
 BOOL Wh_ModInit(void)
 {
     HMODULE hExplorer = GetModuleHandleW(NULL);
@@ -137,12 +164,6 @@ BOOL Wh_ModInit(void)
             {L"private: void " SCALCON L" CTaskBtnGroup::_DrawBar(struct HDC__ *,struct BUTTONRENDERINFO const &,struct BUTTONRENDERINFOSTATES const &)"},
             (void **)&CTaskBtnGroup__DrawBar_orig,
             (void *)CTaskBtnGroup__DrawBar_hook,
-            FALSE,
-        },
-        {
-            {L"public: virtual long __cdecl CTaskBtnGroup::SetLocation(int,int,struct tagRECT const *)"},
-            (void **)&CTaskBtnGroup_SetLocation_orig,
-            (void *)CTaskBtnGroup_SetLocation_hook,
             FALSE,
         },
     };
